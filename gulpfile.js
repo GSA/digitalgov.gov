eb86f8d0e1f6b1c72bc1652e05a010249a01c28c
Jeremy Zilar <jeremyzilar@gmail.com>
2017-04-09 13:28:22 -0400

    Cleaning up the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 var child = require('child_process');
 var gutil = require('gulp-util');
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var jshint = require('gulp-jshint');
-var cleanCSS = require('gulp-clean-css');
 var notify = require("gulp-notify");
 
 var paths = {
@@ -11,28 +8,9 @@ var paths = {
  js: ['assets/js/main.js'],
 };
 
-gulp.task('default',['watch']);
-
-gulp.task('jshint', function() {
-  return gulp.src(paths.js)
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
-});
-
-gulp.task('sass', function(){
-  return gulp.src(paths.scss)
-	.pipe(sass()) // Using gulp-sass
-	.pipe(cleanCSS({compatibility: 'ie8'}))
-	.pipe(gulp.dest('assets/css/'))
-  .pipe(notify({
-    title: 'Done!',
-    message: "<%= file.relative %>",
-    // 'sound': 'Hero' //Look in /System/Library/Sounds for other sounds
-    'sound': 'Pop' 
-  }));
-});
 
 // Running Jekyll via Gulp
+// via https://aaronlasseigne.com/2016/02/03/using-gulp-with-jekyll/
 gulp.task('jekyll', () => {
   const jekyll = child.spawn('jekyll', ['serve',
     '--watch',
@@ -55,11 +33,11 @@ gulp.task('jekyll', () => {
   jekyll.stderr.on('data', jekyllLogger);
 });
 
-// Gulp watch
-gulp.task('watch', function(){
-  gulp.watch('assets/scss/*.scss', ['sass']); 
-  gulp.watch('assets/scss/*/*.scss', ['sass']); 
-  gulp.watch('assets/js/*.js', ['jshint']);
-})
+// // Gulp watch
+// gulp.task('watch', function(){
+//   gulp.watch('assets/scss/*.scss', ['sass']); 
+//   gulp.watch('assets/scss/*/*.scss', ['sass']); 
+//   gulp.watch('assets/js/*.js', ['jshint']);
+// })
 
-gulp.task('default', ['sass', 'jekyll', 'watch']);
+gulp.task('default', ['jekyll']);
