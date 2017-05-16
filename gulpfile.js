c079b6c23c6bbe868047f0fa509c4adb78007761
Dan O. Williams <dan.o.williams@gmail.com>
2017-05-16 17:28:03 -0400

    added rudimentary browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,14 @@ var paths = {
  js: ['assets/js/main.js'],
 };
 
+const browserSync = require('browser-sync').create();
+const siteRoot = '_site';
+
 
 // Running Jekyll via Gulp
 // via https://aaronlasseigne.com/2016/02/03/using-gulp-with-jekyll/
 gulp.task('jekyll', () => {
-  const jekyll = child.spawn('jekyll', ['serve',
+  const jekyll = child.spawn('jekyll', ['build',
     '--watch',
     '--incremental',
     '--drafts'
@@ -27,17 +30,27 @@ gulp.task('jekyll', () => {
     title: 'Done!',
     message: "<%= file.relative %>",
     // 'sound': 'Hero' //Look in /System/Library/Sounds for other sounds
-    'sound': 'Pop' 
+    'sound': 'Pop'
   });
   jekyll.stdout.on('data', jekyllLogger);
   jekyll.stderr.on('data', jekyllLogger);
 });
 
+gulp.task('serve', () => {
+  browserSync.init({
+    files: [siteRoot + '/**'],
+    port: 4000,
+    server: {
+      baseDir: siteRoot
+    }
+  });
+});
+
 // // Gulp watch
 // gulp.task('watch', function(){
 //   gulp.watch('assets/scss/*.scss', ['sass']); 
-//   gulp.watch('assets/scss/*/*.scss', ['sass']); 
+//   gulp.watch('assets/scss/*/*.scss', ['sass']);
 //   gulp.watch('assets/js/*.js', ['jshint']);
 // })
 
-gulp.task('default', ['jekyll']);
+gulp.task('default', ['jekyll', 'serve']);
