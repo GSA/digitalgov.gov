622fe11f5911ea385569521e051d9e55ce060513
Dan O. Williams <dan.o.williams@gmail.com>
2017-05-22 20:38:06 -0700

    added process for gulp images

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ var gulp = require('gulp');
 var notify = require("gulp-notify");
 
 var sass = require('gulp-sass'),
+    del = require('del'),
     autoprefixer = require('gulp-autoprefixer'),
 		rename = require('gulp-rename'),
     cleanCSS = require('gulp-clean-css'),
     combineMq = require('gulp-combine-mq'),
     strip = require('gulp-strip-css-comments'),
     size = require('gulp-size'),
+    responsive = require('gulp-responsive'),
     browserSync = require('browser-sync').create();
 
 var supportedBrowsers = [
@@ -35,6 +37,155 @@ var messages = {
 
 const siteRoot = '_site';
 
+gulp.task('build-images', function () {
+  return gulp.src('assets/img/_inbox/*.{png,jpg}')
+    .pipe(gulp.dest('_site/assets/img/raw'))
+    .pipe(gulp.dest('assets/img/raw'))
+    .pipe(responsive({
+      '*': [{
+        // image-xs.jpg is 200 pixels wide
+        width: 200,
+        rename: {
+          suffix: '-200w',
+          extname: '.jpg',
+        },
+      }, {
+        // image-400w.jpg is 400 pixels wide
+        width: 400,
+        rename: {
+          suffix: '-400w',
+          extname: '.jpg',
+        },
+        withoutEnlargement: true,
+      }, {
+        // image-600w.jpg is 800 pixels wide
+        width: 600,
+        rename: {
+          suffix: '-600w',
+          extname: '.jpg',
+        },
+        withoutEnlargement: true,
+      }, {
+        // image-md.jpg is 600 pixels wide
+        width: 800,
+        rename: {
+          suffix: '-800w',
+          extname: '.jpg',
+        },
+        withoutEnlargement: true,
+      }, {
+        // image-1200w.jpg is 1200 pixels wide
+        width: 1200,
+        rename: {
+          suffix: '-1200w',
+          extname: '.jpg',
+        },
+        withoutEnlargement: true,
+      }, {
+        // image-1600w.jpg is 600 pixels wide
+        width: 1600,
+        rename: {
+          suffix: '-1600w',
+          extname: '.jpg',
+        },
+        withoutEnlargement: true,
+      }, {
+        // image-2400w.jpg is 1200 pixels wide
+        width: 2400,
+        rename: {
+          suffix: '-2400w',
+          extname: '.jpg',
+        },
+        withoutEnlargement: true,
+      }, {
+        // image-200w.webp is 200 pixels wide
+        width: 200,
+        rename: {
+          suffix: '-200w',
+          extname: '.webp',
+        },
+      }, {
+        // image-400w.webp is 400 pixels wide
+        width: 400,
+        rename: {
+          suffix: '-400w',
+          extname: '.webp',
+        },
+        withoutEnlargement: true,
+      }, {
+        // image-600w.webp is 400 pixels wide
+        width: 600,
+        rename: {
+          suffix: '-600w',
+          extname: '.webp',
+        },
+        withoutEnlargement: true,
+      }, {
+        // image-800w.webp is 400 pixels wide
+        width: 800,
+        rename: {
+          suffix: '-800w',
+          extname: '.webp',
+        },
+        withoutEnlargement: true,
+      }, {
+        // image-1200w.webp is 400 pixels wide
+        width: 1200,
+        rename: {
+          suffix: '-1200w',
+          extname: '.webp',
+        },
+        withoutEnlargement: true,
+      }, {
+        // image-1600w.webp is 400 pixels wide
+        width: 1600,
+        rename: {
+          suffix: '-1600w',
+          extname: '.webp',
+        },
+        withoutEnlargement: true,
+      }, {
+        // image-1600w.webp is 400 pixels wide
+        width: 2400,
+        rename: {
+          suffix: '-2400w',
+          extname: '.webp',
+        },
+        withoutEnlargement: true,
+      }],
+    }, {
+      // Global configuration for all images
+      // The output quality for JPEG, WebP and TIFF output formats
+      quality: 80,
+      // Use progressive (interlace) scan for JPEG and PNG output
+      progressive: true,
+      // Strip all metadata
+      withMetadata: false,
+      // Do not emit the error when image is enlarged.
+      errorOnEnlargement: false,
+      errorOnUnusedConfig: false,
+      silent: true,
+    }))
+    .pipe(gulp.dest('_site/assets/img/dist'))
+    .pipe(gulp.dest('assets/img/dist'));
+});
+
+gulp.task('img', ['build-images'], function () {
+  return del([
+    'assets/img/_inbox/*'
+  ]);
+});
+
+gulp.task('img-notify', ['img'], function () {
+  return gulp.src("/")
+    .pipe(notify({
+      "title": "digital.gov",
+      "subtitle": "Responsive images built.",
+      "message": "Project reloaded.",
+      "sound": "Pop" // case sensitive
+    }))
+    .pipe(browserSync.stream());
+});
 
 gulp.task('jekyll-build', function (done) {
     browserSync.notify(messages.jekyllBuild);
@@ -137,12 +288,14 @@ gulp.task('watch', function () {
       '_includes/**/*',
       '_layouts/**/*',
       '_posts/**/*',
-      'assets/img/**/*',
       'content/**/*'
     ], ['jekyll-rebuild']);
     gulp.watch([
       '_data/**/*'
     ], ['jekyll-rebuild-complete']);
+    gulp.watch([
+      'assets/img/_inbox/**/*',
+    ], ['img-notify']);
 });
 
-gulp.task('default', ['jekyll-first-build', 'sass', 'watch']);
+gulp.task('default', ['jekyll-first-build', 'img-notify', 'sass', 'watch']);
