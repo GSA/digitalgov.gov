c389129a2bc413df1b4878d7503d8edb2b9b94b3
Dan O. Williams <dan.o.williams@gmail.com>
2017-05-22 23:30:11 -0700

    better flow for img processing

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var notify = require("gulp-notify");
 
 var sass = require('gulp-sass'),
+    watch = require('gulp-watch'),
     del = require('del'),
     autoprefixer = require('gulp-autoprefixer'),
 		rename = require('gulp-rename'),
@@ -12,6 +13,7 @@ var sass = require('gulp-sass'),
     strip = require('gulp-strip-css-comments'),
     size = require('gulp-size'),
     responsive = require('gulp-responsive'),
+    vinylPaths = require('vinyl-paths'),
     browserSync = require('browser-sync').create();
 
 var supportedBrowsers = [
@@ -37,154 +39,133 @@ var messages = {
 
 const siteRoot = '_site';
 
-gulp.task('build-images', function () {
-  return gulp.src('assets/img/_to-process/*.{png,jpg}')
-    .pipe(gulp.dest('_site/assets/img/raw'))
-    .pipe(gulp.dest('assets/img/raw'))
-    .pipe(responsive({
-      '*': [{
-        // image-xs.jpg is 200 pixels wide
-        width: 200,
-        rename: {
-          suffix: '-200w',
-          extname: '.jpg',
-        },
+gulp.task('img', function () {
+  return watch('assets/img/_to-process/*.{png,jpg}', { ignoreInitial: false }, function () {
+    gulp.src('assets/img/_to-process/*.{png,jpg}')
+      .pipe(vinylPaths(del))
+      .pipe(gulp.dest('_site/assets/img/raw'))
+      .pipe(gulp.dest('assets/img/raw'))
+      .pipe(responsive({
+        '*': [{
+          // image-xs.jpg is 200 pixels wide
+          width: 200,
+          rename: {
+            suffix: '-200w',
+            extname: '.jpg',
+          },
+        }, {
+          // image-400w.jpg is 400 pixels wide
+          width: 400,
+          rename: {
+            suffix: '-400w',
+            extname: '.jpg',
+          },
+        }, {
+          // image-600w.jpg is 800 pixels wide
+          width: 600,
+          rename: {
+            suffix: '-600w',
+            extname: '.jpg',
+          },
+        }, {
+          // image-md.jpg is 600 pixels wide
+          width: 800,
+          rename: {
+            suffix: '-800w',
+            extname: '.jpg',
+          },
+        }, {
+          // image-1200w.jpg is 1200 pixels wide
+          width: 1200,
+          rename: {
+            suffix: '-1200w',
+            extname: '.jpg',
+          },
+        }, {
+          // image-1600w.jpg is 600 pixels wide
+          width: 1600,
+          rename: {
+            suffix: '-1600w',
+            extname: '.jpg',
+          },
+        }, {
+          // image-2400w.jpg is 1200 pixels wide
+          width: 2400,
+          rename: {
+            suffix: '-2400w',
+            extname: '.jpg',
+          },
+        }, {
+          // image-200w.webp is 200 pixels wide
+          width: 200,
+          rename: {
+            suffix: '-200w',
+            extname: '.webp',
+          },
+        }, {
+          // image-400w.webp is 400 pixels wide
+          width: 400,
+          rename: {
+            suffix: '-400w',
+            extname: '.webp',
+          },
+        }, {
+          // image-600w.webp is 400 pixels wide
+          width: 600,
+          rename: {
+            suffix: '-600w',
+            extname: '.webp',
+          },
+        }, {
+          // image-800w.webp is 400 pixels wide
+          width: 800,
+          rename: {
+            suffix: '-800w',
+            extname: '.webp',
+          },
+        }, {
+          // image-1200w.webp is 400 pixels wide
+          width: 1200,
+          rename: {
+            suffix: '-1200w',
+            extname: '.webp',
+          },
+        }, {
+          // image-1600w.webp is 400 pixels wide
+          width: 1600,
+          rename: {
+            suffix: '-1600w',
+            extname: '.webp',
+          },
+        }, {
+          // image-1600w.webp is 400 pixels wide
+          width: 2400,
+          rename: {
+            suffix: '-2400w',
+            extname: '.webp',
+          },
+        }],
       }, {
-        // image-400w.jpg is 400 pixels wide
-        width: 400,
-        rename: {
-          suffix: '-400w',
-          extname: '.jpg',
-        },
-        withoutEnlargement: true,
-      }, {
-        // image-600w.jpg is 800 pixels wide
-        width: 600,
-        rename: {
-          suffix: '-600w',
-          extname: '.jpg',
-        },
-        withoutEnlargement: true,
-      }, {
-        // image-md.jpg is 600 pixels wide
-        width: 800,
-        rename: {
-          suffix: '-800w',
-          extname: '.jpg',
-        },
-        withoutEnlargement: true,
-      }, {
-        // image-1200w.jpg is 1200 pixels wide
-        width: 1200,
-        rename: {
-          suffix: '-1200w',
-          extname: '.jpg',
-        },
-        withoutEnlargement: true,
-      }, {
-        // image-1600w.jpg is 600 pixels wide
-        width: 1600,
-        rename: {
-          suffix: '-1600w',
-          extname: '.jpg',
-        },
-        withoutEnlargement: true,
-      }, {
-        // image-2400w.jpg is 1200 pixels wide
-        width: 2400,
-        rename: {
-          suffix: '-2400w',
-          extname: '.jpg',
-        },
-        withoutEnlargement: true,
-      }, {
-        // image-200w.webp is 200 pixels wide
-        width: 200,
-        rename: {
-          suffix: '-200w',
-          extname: '.webp',
-        },
-      }, {
-        // image-400w.webp is 400 pixels wide
-        width: 400,
-        rename: {
-          suffix: '-400w',
-          extname: '.webp',
-        },
-        withoutEnlargement: true,
-      }, {
-        // image-600w.webp is 400 pixels wide
-        width: 600,
-        rename: {
-          suffix: '-600w',
-          extname: '.webp',
-        },
-        withoutEnlargement: true,
-      }, {
-        // image-800w.webp is 400 pixels wide
-        width: 800,
-        rename: {
-          suffix: '-800w',
-          extname: '.webp',
-        },
-        withoutEnlargement: true,
-      }, {
-        // image-1200w.webp is 400 pixels wide
-        width: 1200,
-        rename: {
-          suffix: '-1200w',
-          extname: '.webp',
-        },
-        withoutEnlargement: true,
-      }, {
-        // image-1600w.webp is 400 pixels wide
-        width: 1600,
-        rename: {
-          suffix: '-1600w',
-          extname: '.webp',
-        },
-        withoutEnlargement: true,
-      }, {
-        // image-1600w.webp is 400 pixels wide
-        width: 2400,
-        rename: {
-          suffix: '-2400w',
-          extname: '.webp',
-        },
-        withoutEnlargement: true,
-      }],
-    }, {
-      // Global configuration for all images
-      // The output quality for JPEG, WebP and TIFF output formats
-      quality: 80,
-      // Use progressive (interlace) scan for JPEG and PNG output
-      progressive: true,
-      // Strip all metadata
-      withMetadata: false,
-      // Do not emit the error when image is enlarged.
-      errorOnEnlargement: false,
-      errorOnUnusedConfig: false,
-      silent: true,
-    }))
-    .pipe(gulp.dest('_site/assets/img/dist'))
-    .pipe(gulp.dest('assets/img/dist'));
-});
-
-gulp.task('img', ['build-images'], function () {
-  return del([
-    'assets/img/_to-process/*'
-  ]);
-});
-
-gulp.task('img-notify', ['img'], function () {
-  return gulp.src("/")
-    .pipe(notify({
-      "title": "digital.gov",
-      "subtitle": "Responsive images built.",
-      "message": "Project reloaded.",
-      "sound": "Pop" // case sensitive
-    }))
-    .pipe(browserSync.stream());
+        // Global configuration for all images
+        quality: 80,
+        progressive: true,
+        withMetadata: false,
+        errorOnUnusedConfig: false,
+        skipOnEnlargement: true,
+        errorOnEnlargement: false,
+        silent: true,
+      }))
+      .pipe(gulp.dest('_site/assets/img/dist'))
+      .pipe(gulp.dest('assets/img/dist'))
+      .pipe(browserSync.stream({once: true}))
+      .pipe(notify({
+        "title": "digital.gov",
+        "subtitle": "Responsive images built.",
+        "message": "Project reloaded.",
+        "onLast": true,
+        "sound": "Pop" // case sensitive
+      }));
+    });
 });
 
 gulp.task('jekyll-build', function (done) {
@@ -293,9 +274,6 @@ gulp.task('watch', function () {
     gulp.watch([
       '_data/**/*'
     ], ['jekyll-rebuild-complete']);
-    gulp.watch([
-      'assets/img/_to-processx/**/*',
-    ], ['img-notify']);
 });
 
-gulp.task('default', ['jekyll-first-build', 'img-notify', 'sass', 'watch']);
+gulp.task('default', ['jekyll-first-build', 'sass', 'img', 'watch']);
