eab92e51900862efd3a3d1213711d3d8c5cdcf56
Dan O. Williams <dan.o.williams@gmail.com>
2017-08-03 13:02:07 -0700

    Clean `tmp` dir

--- gulpfile.js
+++ gulpfile.js
@@ -23,99 +23,187 @@ var gulp          = require("gulp"),
     cp            = require('child_process');
 
 
+
+    // - - - - - - - - - - - - - - - - -
+    // Create responsive variants
+    gulp.task("create-variants", function (done) {
+      del(['static/_uploaded/**/*']);
+      return gulp.src("static/__inbox/**/*")
+        .pipe(vinylPaths(del))
+        // Add the original to static/img
+        .pipe(gulp.dest("static/img/"))
+        // Create responsive variants
+        .pipe(responsive({
+          '*': [{
+            width: 200,
+            rename: {
+              suffix: '__w200',
+            },
+          }, {
+            width: 200,
+            rename: {
+              suffix: '__bu',
+              extname: '.jpg',
+            },
+            quality: 20,
+            blur: true,
+          }, {
+            width: 200,
+            rename: {
+              suffix: '__w200',
+              extname: '.webp',
+            },
+          }, {
+            width: 200,
+            grayscale: true,
+            rename: {
+              suffix: '__gray__w200',
+            },
+          }, {
+            width: 400,
+            rename: {
+              suffix: '__w400',
+            },
+          }, {
+            width: 400,
+            rename: {
+              suffix: '__w400',
+              extname: '.webp',
+            },
+          }, {
+            width: 400,
+            grayscale: true,
+            rename: {
+              suffix: '__gray__w400',
+            },
+          }, {
+            width: 600,
+            rename: {
+              suffix: '__w600',
+            },
+          }, {
+            width: 600,
+            rename: {
+              suffix: '__w600',
+              extname: '.webp',
+            },
+          }, {
+            width: 600,
+            grayscale: true,
+            rename: {
+              suffix: '__gray__w600',
+            },
+          }, {
+            width: 800,
+            rename: {
+              suffix: '__w800',
+            },
+          }, {
+            width: 800,
+            rename: {
+              suffix: '__w800',
+              extname: '.webp',
+            },
+          }, {
+            width: 800,
+            grayscale: true,
+            rename: {
+              suffix: '__gray__w800',
+            },
+          }, {
+            width: 1200,
+            rename: {
+              suffix: '__w1200',
+            },
+          }, {
+            width: 1200,
+            rename: {
+              suffix: '__w1200',
+              extname: '.webp',
+            },
+          }, {
+            width: 1200,
+            grayscale: true,
+            rename: {
+              suffix: '__gray__w1200',
+            },
+          }, {
+            width: 1600,
+            rename: {
+              suffix: '__w1600',
+            },
+          }, {
+            width: 1600,
+            rename: {
+              suffix: '__w1600',
+              extname: '.webp',
+            },
+          }, {
+            width: 1600,
+            grayscale: true,
+            rename: {
+              suffix: '__gray__w1600',
+            },
+          }, {
+            width: 2400,
+            rename: {
+              suffix: '__w2400',
+            },
+          }, {
+            width: 2400,
+            rename: {
+              suffix: '__w2400',
+              extname: '.webp',
+            },
+          }, {
+            width: 2400,
+            grayscale: true,
+            rename: {
+              suffix: '__gray__w2400',
+            },
+          }, {
+            // This blank entry makes a copy of the original image
+          }],
+        }, {
+          // Global configuration for all images
+          quality: 80,
+          progressive: true,
+          withMetadata: false,
+          errorOnUnusedConfig: false,
+          skipOnEnlargement: true,
+          errorOnEnlargement: false,
+          silent: true,
+        }))
+        .pipe(gulp.dest("static/tmp/"));
+    });
+
 // - - - - - - - - - - - - - - - - -
-// Process and upload images
-gulp.task("images", function () {
+// Upload images
+gulp.task("upload", ["create-variants"], function (done) {
+  return gulp.src("static/tmp/**/*")
+    // output files to the /content/aws dir
+    .pipe(s3({
+      Bucket: 'digitalgov',   //  Required
+      ACL:    'public-read'   //  Needs to be user-defined
+    }, {
+      // S3 Constructor Options, ie:
+      maxRetries: 5
+    }));
+  del(['static/tmp/**']);
+});
+
+// - - - - - - - - - - - - - - - - -
+// Upload images
+gulp.task("clean", ["upload"], function (done) {
+  return del(['static/tmp/**']);
+});
+
+// - - - - - - - - - - - - - - - - -
+// Process images
+gulp.task("watch-img", ["clean"], function (done) {
   return watch('static/__inbox/**/*', {
     ignoreInitial: false,
     events: ['add', 'change']
-  }, function () {
-    del(['static/_done/**/*']);
-    gulp.src("static/__inbox/**/*")
-      .pipe(vinylPaths(del))
-/*
-          // append a hash to the filename (-NNNNN)
-          .pipe(hash())
-*/
-      .pipe(gulp.dest("static/_done/"))
-      .pipe(responsive({
-        '*': [{
-          // image-xs.jpg is 200 pixels wide
-          width: 200,
-          rename: {
-            suffix: '__w200',
-            extname: '.jpg',
-          },
-        }, {
-          // image-400w.jpg is 400 pixels wide
-          width: 400,
-          rename: {
-            suffix: '__w400',
-            extname: '.jpg',
-          },
-        }, {
-          // image-600w.jpg is 800 pixels wide
-          width: 600,
-          rename: {
-            suffix: '__w600',
-            extname: '.jpg',
-          },
-        }, {
-          // image-md.jpg is 600 pixels wide
-          width: 800,
-          rename: {
-            suffix: '__w800',
-            extname: '.jpg',
-          },
-        }, {
-          // image-1200w.jpg is 1200 pixels wide
-          width: 1200,
-          rename: {
-            suffix: '__w1200',
-            extname: '.jpg',
-          },
-        }, {
-          // image-1600w.jpg is 600 pixels wide
-          width: 1600,
-          rename: {
-            suffix: '__w1600',
-            extname: '.jpg',
-          },
-        }, {
-          // image-2400w.jpg is 1200 pixels wide
-          width: 2400,
-          rename: {
-            suffix: '__w2400',
-            extname: '.jpg',
-          },
-  //        }, {
-  //          // image-max.jpg is original width
-  //          rename: {
-  //            suffix: '-max',
-  //            extname: '.jpg',
-  //          },
-        }],
-      }, {
-        // Global configuration for all images
-        quality: 80,
-        progressive: true,
-        withMetadata: false,
-        errorOnUnusedConfig: false,
-        skipOnEnlargement: true,
-        errorOnEnlargement: false,
-        silent: true,
-  //        flatten: true,
-  //        background: "#fff",
-      }))
-      .pipe(gulp.dest("static/_done/"))
-      // output files to the /content/aws dir
-      .pipe(s3({
-        Bucket: 'digitalgov',   //  Required
-        ACL:    'public-read'   //  Needs to be user-defined
-      }, {
-        // S3 Constructor Options, ie:
-        maxRetries: 5
-      }));
   });
 });
 
@@ -125,9 +213,9 @@ gulp.task("images", function () {
 // - - - - - - - - - - - - - - - - -
 // Watch asset folder for changes
 gulp.task("watch", function () {
-  gulp.watch("static/__inbox/**/*", ["images"])
+  gulp.watch("static/__inbox/**/*", ["watch-img"])
 })
 
 // - - - - - - - - - - - - - - - - -
 // Set watch as default task
-gulp.task("default", ["images", "watch"])
+gulp.task("default", ["watch-img", "watch"])
