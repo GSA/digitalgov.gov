9ffbc33cf725bbfe677eb6a28699a86f3e0fa3f2
Jeremy Zilar <jeremyzilar@gmail.com>
2017-08-09 22:45:26 -0400

    adding post, page and events to editor

--- themes/digital.gov/static/js/front-matter.js
+++ themes/digital.gov/static/js/front-matter.js
@@ -1,6 +1,19 @@
 jQuery(document).ready(function($) {
 
 
+  $( ".btn-group .btn" ).click(function() {
+    $( ".btn-group .btn" ).removeClass( 'selected' );
+    $( this ).addClass( 'selected' );
+    get_matter_data();
+  });
+  
+  function get_post_type(){
+    var post_type = $( '.btn-group .selected' ).attr( 'data-type' );
+    return post_type;
+  }
+
+
+
   function curr_date(){
     var d = new Date();
     var month = d.getMonth()+1;
@@ -15,17 +28,22 @@ jQuery(document).ready(function($) {
 
   $('input[name="m_date"]').val(curr_date());
 
-  $( "#matter-maker input" ).keyup(function( event ) {
+
+  function get_matter_data(){
     var data = {};
     $('#matter-maker').serializeArray().map(function(x){
       data[x.name] = x.value;
     });
     setTimeout(function() {
-      console.log(data);
       print_matter(data);
     }, 400);
+  }
 
+  $( "#matter-maker input" ).keyup(function( event ) {
+    get_matter_data();
   });
+
+
 
   var entityPattern = /[&<>"'`=:*?$%\/]/g;
   var entityMap = {
@@ -62,6 +80,17 @@ jQuery(document).ready(function($) {
     return filename;
   }
 
+  function matter_url(d,t) {
+    var date = d.match(/^[^\s]+/);
+    date = date[0].replace(/-/g, '/');
+    t = t.replace(small_words, '');
+    t = t.replace(entityPattern, ' ').trim();
+    t = t.replace(/\s+/g,' ').trim();
+    var slug = t.replace(/[^a-z0-9]/gi, '-').toLowerCase();
+    var f = '/'+date+'/'+slug;
+    return f;
+  }
+
   function list_items(d) {
     var list = d.split(',');
     $item = '';
@@ -74,22 +103,59 @@ jQuery(document).ready(function($) {
 
 
   function print_matter(data){
-    var matter = [
-      "---",
-        "date: " + data['m_date'],
-        "title: '" + escapeHtml(data['m_title']) + "'",
-        "summary: '" + escapeHtml(data['m_summary']) + "'",
-        "authors: " + list_items(data['m_author']),
-        "categories: " + list_items(data['m_categories']),
-        "tag: " + list_items(data['m_tag']),
-      "---",
-      ,
-      "***Paste content here. Delete this line***"
-    ].join("\n");
+    var post_type = get_post_type();
+    console.log(post_type);
+    
+    if (post_type == 'post') {
+      var matter = [
+        "---",
+          "url: " + matter_url(data['m_date'], data['m_title']),
+          "date: " + data['m_date'],
+          "title: '" + escapeHtml(data['m_title']) + "'",
+          "summary: '" + escapeHtml(data['m_summary']) + "'",
+          "authors: " + list_items(data['m_author']),
+          "categories: " + list_items(data['m_categories']),
+          "tag: " + list_items(data['m_tag']),
+        "---",
+        ,
+        "***Paste content here. Delete this line***"
+      ].join("\n");
+      var body = encodeURIComponent(matter);
+      var newfile = 'https://github.com/GSA/digital.gov/new/demo/content/posts/draft?filename='+filename(data['m_date'], data['m_title'])+'&value='+body;
+
+    } else if (post_type == 'page') {
+      var matter = [
+        "---",
+          "url: " + matter_url(data['m_date'], data['m_title']),
+          "date: " + data['m_date'],
+          "title: '" + escapeHtml(data['m_title']) + "'",
+          "summary: '" + escapeHtml(data['m_summary']) + "'",
+        "---",
+        ,
+        "***Paste content here. Delete this line***"
+      ].join("\n");
+      var body = encodeURIComponent(matter);
+      var newfile = 'https://github.com/GSA/digital.gov/new/demo/content/docs/draft?filename='+filename(data['m_date'], data['m_title'])+'&value='+body;
+
+    } else if (post_type == 'event') {
+      var matter = [
+        "---",
+          "url: " + matter_url(data['m_date'], data['m_title']),
+          "date: " + data['m_date'],
+          "title: '" + escapeHtml(data['m_title']) + "'",
+          "summary: '" + escapeHtml(data['m_summary']) + "'",
+          "categories: " + list_items(data['m_categories']),
+          "tag: " + list_items(data['m_tag']),
+        "---",
+        ,
+        "***Paste content here. Delete this line***"
+      ].join("\n");
+      var body = encodeURIComponent(matter);
+      var newfile = 'https://github.com/GSA/digital.gov/new/demo/content/events/draft?filename='+filename(data['m_date'], data['m_title'])+'&value='+body;
+    }
+    
     $('#post-matter').text(matter);
     $('#filename').text(filename(data['m_date'], data['m_title']));
-    var body = encodeURIComponent(matter);
-    var newfile = 'https://github.com/GSA/digital.gov/new/demo/content/posts/draft?filename='+filename(data['m_date'], data['m_title'])+'&value='+body;
     $('#newfile').attr('href', newfile);
   }
 
