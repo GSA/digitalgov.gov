4f173528906487cdc5e9c7bb155649a8c7cd0ab5
Jeremy Zilar <jeremyzilar@gmail.com>
2017-08-29 14:03:53 -0400

    Edit post page template

--- themes/digital.gov/static/js/front-matter.js
+++ themes/digital.gov/static/js/front-matter.js
@@ -1,5 +1,45 @@
 jQuery(document).ready(function($) {
 
+
+  function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
+
+  function get_edit_file_path(){
+
+    var edit = getParameterByName('edit');
+    var edit = edit.replace(root_url, ''); // remove the trailing slash
+    var edit = edit.replace(/\/$/, '') + '.md'; // remove the trailing slash
+    var edit = edit.replace(/\//g, '-');
+    console.log(edit);
+    // digitalgov.gov/content/posts/2012-02-22-epa-indoor-airplus-app.md
+    var path = 'https://api.github.com/repos/GSA/digitalgov.gov/contents/content/posts/'+edit;
+    console.log(path);
+    return path;
+  }
+
+
+
+  var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+
+  var github_path = get_edit_file_path();
+
+  $.ajax({
+    url: github_path,
+    dataType: 'jsonp',
+    success: function(results) {
+      var content = Base64.decode(results.data.content);
+      console.log(content);
+      $('.matter-body pre').text(content);
+    }
+  });
+
   // Step 1. Add into this string the characters to look for
   var entityPattern = /[&<>"'`)(=+:*@.?$%\/]/g;
   // Step 2. Add a new line that contains the HTML character and the entity that you want it transformed into
