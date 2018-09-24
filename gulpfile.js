fb1702ca0018d715d9c04bd83e5f5430eaa20bf7
Jeremy Zilar <jeremyzilar@gmail.com>
2018-09-24 17:24:13 -0400

    mobile nav roughed out

--- gulpfile.js
+++ gulpfile.js
@@ -18,15 +18,19 @@ const USWDS_SRC         = 'node_modules/uswds/dist';
 // Project Sass source directory
 const PROJECT_SASS_SRC  = './themes/digital.gov/src/uswds';
 
+// Project JS source directory
+const PROJECT_JS_SRC  = './themes/digital.gov/src/js';
+
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 gulp.task('watch', ['build-sass'], function (){
   gulp.watch(`${PROJECT_SASS_SRC}/*.scss`, ['build-sass'])
   gulp.watch(`${PROJECT_SASS_SRC}/scss/*.scss`, ['build-sass'])
+  gulp.watch(`${PROJECT_JS_SRC}/*.js`, ['compile'])
   gulp.watch(`${USWDS_SRC}/scss/*.scss`, ['copy-uswds-assets','build-sass']);
 });
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // Set watch as default task
 // gulp.task('default', ['watch', 'copy-uswds-assets', 'copy-uswds-fonts', 'build-sass', 'fontawesome', 'compile']);
-gulp.task('default', ['watch', 'build-sass']);
+gulp.task('default', ['watch', 'compile','build-sass']);
--- themes/digital.gov/src/js/overlay.js
+++ themes/digital.gov/src/js/overlay.js
@@ -3,10 +3,16 @@ jQuery(document).ready(function($) {
 	$("#overlay .close").click(function() {
 		$("#overlay").toggle();
 	});
-	
+
 	$(document).bind('keyup', function (evt) {
 	  if (evt.keyCode == 191){
 			$("#overlay").toggle();
+		}
+	});
+
+	$(document).bind('keyup', function (evt) {
+	  if (evt.keyCode == 27){
+			$("#mobile-nav").toggle();
 		}
 	});
 
