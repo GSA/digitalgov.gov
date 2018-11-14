8b40faca45a6b242b9b22aa22da9ac251bfb0dfe
Jeremy Zilar <jeremyzilar@gmail.com>
2018-11-14 16:58:53 -0500

    optimizing

--- themes/digital.gov/src/js/clicks.js
+++ themes/digital.gov/src/js/clicks.js
@@ -11,21 +11,16 @@ jQuery(document).ready(function($) {
 				var api = 'https://go.usa.gov/api/clicks.json?login=jeremyz&apiKey=d0ac464a2dfad044147e9537fd51503b&shortUrl=' + encodeURI(short_url);
 				$.getJSON( api, function( data ) {
 					$.each( data.response.data.entry, function( key, value ) {
-						var clicks = $(article).find('.clicks span').text();
-						console.log("clicks");
-						console.log(clicks);
+						var clicks_element = $(article).find('.clicks span');
+						var clicks = $(clicks_element).text();
 						var current_clicks = value.user_clicks;
-						console.log("current_clicks");
-						console.log(current_clicks);
 						var interval = setInterval(function() {
-			        // $('#number').text(number);
 			        if (clicks >= current_clicks) clearInterval(interval);
 			        clicks++;
-							$(article).find('.clicks span').html(clicks);
+							$(clicks_element).html(clicks);
 				    }, 30);
 
 					});
-					// callback(data.response.data.entry);
 				});
 			});
 	  });
