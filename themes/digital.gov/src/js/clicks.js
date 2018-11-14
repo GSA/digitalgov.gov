553c89128b85cb615a571e4e52225d6afad09b48
Jeremy Zilar <jeremyzilar@gmail.com>
2018-11-14 16:56:10 -0500

    adding counter to clicks

--- themes/digital.gov/src/js/clicks.js
+++ themes/digital.gov/src/js/clicks.js
@@ -11,8 +11,19 @@ jQuery(document).ready(function($) {
 				var api = 'https://go.usa.gov/api/clicks.json?login=jeremyz&apiKey=d0ac464a2dfad044147e9537fd51503b&shortUrl=' + encodeURI(short_url);
 				$.getJSON( api, function( data ) {
 					$.each( data.response.data.entry, function( key, value ) {
-						console.log(value.user_clicks);
-						$(article).find('.clicks span').html(value.user_clicks);
+						var clicks = $(article).find('.clicks span').text();
+						console.log("clicks");
+						console.log(clicks);
+						var current_clicks = value.user_clicks;
+						console.log("current_clicks");
+						console.log(current_clicks);
+						var interval = setInterval(function() {
+			        // $('#number').text(number);
+			        if (clicks >= current_clicks) clearInterval(interval);
+			        clicks++;
+							$(article).find('.clicks span').html(clicks);
+				    }, 30);
+
 					});
 					// callback(data.response.data.entry);
 				});
