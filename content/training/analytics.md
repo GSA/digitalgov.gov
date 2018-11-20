52319b56040a558781587476b4f5b87f7204da76
Jeremy Zilar <jeremyzilar@gmail.com>
2018-11-20 15:58:22 -0500

    Training pages styles and function

--- content/training/analytics.md
+++ content/training/analytics.md
@@ -3,7 +3,9 @@ url: /training/analytics
 date: 2018-10-30 12:13:13 -0400
 title: Analytics
 summary: 'summary goes here...'
-
+topics:
+  - analytics
+  - uswds
 ---
 
 analytics trainings...
--- themes/digital.gov/src/js/video-cards.js
+++ themes/digital.gov/src/js/video-cards.js
@@ -1,23 +1,34 @@
 /* Light YouTube Embeds by @labnol */
 /* Web: http://labnol.org/?p=27941 */
 
-document.addEventListener("DOMContentLoaded",
-		function() {
-				var div, n,
-						v = document.getElementsByClassName("youtube-player");
-				for (n = 0; n < v.length; n++) {
-						div = document.createElement("div");
-						div.setAttribute("data-id", v[n].dataset.id);
-						div.innerHTML = labnolThumb(v[n].dataset.id);
-						div.onclick = labnolIframe;
-						v[n].appendChild(div);
-				}
-		});
+document.addEventListener("DOMContentLoaded", function() {
+	var div, n,
+		v = document.getElementsByClassName("youtube-player");
+		c = document.getElementsByClassName("youtube-card");
+	for (n = 0; n < v.length; n++) {
+		div = document.createElement("div");
+		div.setAttribute("data-id", v[n].dataset.id);
+		div.innerHTML = labnolThumb(v[n].dataset.id);
+		div.onclick = labnolIframe;
+		v[n].appendChild(div);
+	}
+	for (n = 0; n < c.length; n++) {
+		div = document.createElement("div");
+		div.setAttribute("data-id", c[n].dataset.id);
+		div.innerHTML = youtube_card(c[n].dataset.id);
+		c[n].appendChild(div);
+	}
+});
+
+function youtube_card(id) {
+	var thumb = '<img src="https://i.ytimg.com/vi/'+id+'/hqdefault.jpg">';
+	return thumb;
+}
 
 function labnolThumb(id) {
-		var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',
-				play = '<div class="play"></div>';
-		return thumb.replace("ID", id) + play;
+	var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',
+			play = '<div class="play"></div>';
+	return thumb.replace("ID", id) + play;
 }
 
 function labnolIframe() {
