ad917c80903f143e160ac716d18a750ecff85a90
Jeremy Zilar <jeremyzilar@gmail.com>
2019-06-28 00:27:32 -0400

    adding gulp github file

---
+++ config/gulp/github.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var git = require('gulp-git');
+
+// Run git add
+// src is the file(s) to add (or ./*)
+gulp.task('add', function(){
+  return gulp.src('./*')
+    .pipe(git.add());
+});
+
+// Run git commit with a computed commit message
+gulp.task('commit', gulp.series('add', function (done){
+  let newVersion;
+  function computeNewVersion() { newVersion = "3.3" }
+  return gulp.src('*')
+    .pipe(computeNewVersion())
+    .pipe(git.commit(() => `new image`));
+}));
+
+// gulp.task("commit", gulp.series('add', function (done) {
+//   return gulp.src('*')
+//     .pipe(git.add());
+// }));
