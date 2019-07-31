4bac869efcac7b8738f88af84bbb6d127d8c919a
Jeremy Zilar <jeremyzilar@gmail.com>
2019-07-31 11:53:19 +0200

    moved watch tasks to the main gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
-require('./config/gulp/images');
-// require('./config/gulp/github');
 var gulp = require("gulp");
+require('./config/gulp/images');
+require('./config/gulp/uswds');
 
+
+
+// Image Processor
 gulp.task("process-img", gulp.series('cleanup'));
+
+// USWDS
+gulp.task('watch-sass', function () {
+  gulp.watch(`${PROJECT_SASS_SRC}/**/*.scss`, gulp.series('build-sass'));
+});
+
+gulp.task('watch', gulp.series('build-sass', 'watch-sass'));
+
+gulp.task('default', gulp.series('watch'));
