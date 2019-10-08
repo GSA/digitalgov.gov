1a36860c1df1dc7d7838a0d9b38afe0aeff0bb60
Jeremy Zilar <jeremy.zilar@gsa.gov>
2019-10-08 17:38:57 -0400

    FIXED the image process!

--- config/gulp/images.js
+++ config/gulp/images.js
@@ -26,10 +26,7 @@ var gulp          = require("gulp"),
     fs            = require('graceful-fs'),
     sizeOf        = require('image-size'),
     dotenv        = require('dotenv').config(),
-    s3config      = {
-                    accessKeyId: process.env.AWS_ACCESSKEY,
-                    secretAccessKey: process.env.AWS_SECRET
-                  },
+    s3config      = JSON.parse(fs.readFileSync('.env')),
     s3            = require('gulp-s3-upload')(s3config),
     cp            = require('child_process');
 
@@ -110,223 +107,231 @@ gulp.task("img-variants", gulp.series('clean-inbox', function (done) {
       var format = get_image_format(file.path);
       var dimensions = sizeOf(file.path);
       fs.writeFile('data/images/'+ uid +'.yml', get_image_data(uid, dimensions.width, dimensions.height, format), function(){
-        console.log('image written');
+        console.log('image file written');
       });
     }))
-    // Create responsive variants
-      .pipe(responsive({
-      '*': [{
-        width: 200,
-        rename: {
-          suffix: '_w200',
-        },
-      }, {
-        width: 200,
-        skipOnEnlargement: false,
-        rename: {
-          suffix: '_bu',
-          extname: '.jpg',
-        },
-        quality: 20,
-        blur: true,
-      }, {
-        width: 200,
-        rename: {
-          suffix: '_w200',
-          extname: '.webp',
-        },
-      }, {
-        width: 200,
-        grayscale: true,
-        rename: {
-          suffix: '_w200bw',
-        },
-      }, {
-        width: 200,
-        grayscale: true,
-        rename: {
-          suffix: '_w200bw',
-          extname: '.webp',
-        },
-      }, {
-        width: 400,
-        rename: {
-          suffix: '_w400',
-        },
-      }, {
-        width: 400,
-        rename: {
-          suffix: '_w400',
-          extname: '.webp',
-        },
-      }, {
-        width: 400,
-        grayscale: true,
-        rename: {
-          suffix: '_w400bw',
-        },
-      }, {
-        width: 400,
-        grayscale: true,
-        rename: {
-          suffix: '_w400bw',
-          extname: '.webp',
-        },
-      }, {
-        width: 600,
-        rename: {
-          suffix: '_w600',
-        },
-      }, {
-        width: 600,
-        rename: {
-          suffix: '_w600',
-          extname: '.webp',
-        },
-      }, {
-        width: 600,
-        grayscale: true,
-        rename: {
-          suffix: '_w600bw',
-        },
-      }, {
-        width: 600,
-        grayscale: true,
-        rename: {
-          suffix: '_w600bw',
-          extname: '.webp',
-        },
-      }, {
-        width: 800,
-        rename: {
-          suffix: '_w800',
-        },
-      }, {
-        width: 800,
-        rename: {
-          suffix: '_w800',
-          extname: '.webp',
-        },
-      }, {
-        width: 800,
-        grayscale: true,
-        rename: {
-          suffix: '_w800bw',
-        },
-      }, {
-        width: 800,
-        grayscale: true,
-        rename: {
-          suffix: '_w800bw',
-          extname: '.webp',
-        },
-      }, {
-        width: 1200,
-        rename: {
-          suffix: '_w1200',
-        },
-      }, {
-        width: 1200,
-        rename: {
-          suffix: '_w1200',
-          extname: '.webp',
-        },
-      }, {
-        width: 1200,
-        grayscale: true,
-        rename: {
-          suffix: '_w1200bw',
-        },
-      }, {
-        width: 1200,
-        grayscale: true,
-        rename: {
-          suffix: '_w1200bw',
-          extname: '.webp',
-        },
-      }, {
-        width: 1600,
-        rename: {
-          suffix: '_w1600',
-        },
-      }, {
-        width: 1600,
-        rename: {
-          suffix: '_w1600',
-          extname: '.webp',
-        },
-      }, {
-        width: 1600,
-        grayscale: true,
-        rename: {
-          suffix: '_w1600bw',
-        },
-      }, {
-        width: 1600,
-        grayscale: true,
-        rename: {
-          suffix: '_w1600bw',
-          extname: '.webp',
-        },
-      }, {
-        width: 2400,
-        rename: {
-          suffix: '_w2400',
-        },
-      }, {
-        width: 2400,
-        rename: {
-          suffix: '_w2400',
-          extname: '.webp',
-        },
-      }, {
-        width: 2400,
-        grayscale: true,
-        rename: {
-          suffix: '_w2400bw',
-        },
-      }, {
-        width: 2400,
-        grayscale: true,
-        rename: {
-          suffix: '_w2400bw',
-          extname: '.webp',
-        },
-      }, {
-        // original -> grayscale
-        grayscale: true,
-        rename: {
-          suffix: '_bw',
-        },
-      }, {
-        // original -> grayscale webp
-        grayscale: true,
-        rename: {
-          suffix: '_bw',
-          extname: '.webp',
-      },
-      }, {
-        // original -> webp
-        rename: {
-          suffix: '',
-          extname: '.webp',
-        },
-      }, {
-        // Empty case to produce a copy of the original
-      }],
-    }, {
-      // Global configuration for all images
-      quality: 80,
-      compressionLevel: 7,
-      progressive: true,
-      withMetadata: false,
-      errorOnUnusedConfig: false,
-      skipOnEnlargement: true,
-      errorOnEnlargement: false,
-      silent: true,
+    .pipe(tap(function (file) {
+      var dir = 'content/images/_working/processed';
+      if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir), function(){
+          console.log('folder written');
+        };
+      }
     }))
-    .pipe(vinylPaths(del))
-    .pipe(gulp.dest("content/images/_working/processed/"));
+    // Create responsive variants
+    .pipe(responsive({
+    '*': [{
+      width: 200,
+      rename: {
+        suffix: '_w200',
+      },
+    }, {
+      width: 200,
+      skipOnEnlargement: false,
+      rename: {
+        suffix: '_bu',
+        extname: '.jpg',
+      },
+      quality: 20,
+      blur: true,
+    }, {
+      width: 200,
+      rename: {
+        suffix: '_w200',
+        extname: '.webp',
+      },
+    }, {
+      width: 200,
+      grayscale: true,
+      rename: {
+        suffix: '_w200bw',
+      },
+    }, {
+      width: 200,
+      grayscale: true,
+      rename: {
+        suffix: '_w200bw',
+        extname: '.webp',
+      },
+    }, {
+      width: 400,
+      rename: {
+        suffix: '_w400',
+      },
+    }, {
+      width: 400,
+      rename: {
+        suffix: '_w400',
+        extname: '.webp',
+      },
+    }, {
+      width: 400,
+      grayscale: true,
+      rename: {
+        suffix: '_w400bw',
+      },
+    }, {
+      width: 400,
+      grayscale: true,
+      rename: {
+        suffix: '_w400bw',
+        extname: '.webp',
+      },
+    }, {
+      width: 600,
+      rename: {
+        suffix: '_w600',
+      },
+    }, {
+      width: 600,
+      rename: {
+        suffix: '_w600',
+        extname: '.webp',
+      },
+    }, {
+      width: 600,
+      grayscale: true,
+      rename: {
+        suffix: '_w600bw',
+      },
+    }, {
+      width: 600,
+      grayscale: true,
+      rename: {
+        suffix: '_w600bw',
+        extname: '.webp',
+      },
+    }, {
+      width: 800,
+      rename: {
+        suffix: '_w800',
+      },
+    }, {
+      width: 800,
+      rename: {
+        suffix: '_w800',
+        extname: '.webp',
+      },
+    }, {
+      width: 800,
+      grayscale: true,
+      rename: {
+        suffix: '_w800bw',
+      },
+    }, {
+      width: 800,
+      grayscale: true,
+      rename: {
+        suffix: '_w800bw',
+        extname: '.webp',
+      },
+    }, {
+      width: 1200,
+      rename: {
+        suffix: '_w1200',
+      },
+    }, {
+      width: 1200,
+      rename: {
+        suffix: '_w1200',
+        extname: '.webp',
+      },
+    }, {
+      width: 1200,
+      grayscale: true,
+      rename: {
+        suffix: '_w1200bw',
+      },
+    }, {
+      width: 1200,
+      grayscale: true,
+      rename: {
+        suffix: '_w1200bw',
+        extname: '.webp',
+      },
+    }, {
+      width: 1600,
+      rename: {
+        suffix: '_w1600',
+      },
+    }, {
+      width: 1600,
+      rename: {
+        suffix: '_w1600',
+        extname: '.webp',
+      },
+    }, {
+      width: 1600,
+      grayscale: true,
+      rename: {
+        suffix: '_w1600bw',
+      },
+    }, {
+      width: 1600,
+      grayscale: true,
+      rename: {
+        suffix: '_w1600bw',
+        extname: '.webp',
+      },
+    }, {
+      width: 2400,
+      rename: {
+        suffix: '_w2400',
+      },
+    }, {
+      width: 2400,
+      rename: {
+        suffix: '_w2400',
+        extname: '.webp',
+      },
+    }, {
+      width: 2400,
+      grayscale: true,
+      rename: {
+        suffix: '_w2400bw',
+      },
+    }, {
+      width: 2400,
+      grayscale: true,
+      rename: {
+        suffix: '_w2400bw',
+        extname: '.webp',
+      },
+    }, {
+      // original -> grayscale
+      grayscale: true,
+      rename: {
+        suffix: '_bw',
+      },
+    }, {
+      // original -> grayscale webp
+      grayscale: true,
+      rename: {
+        suffix: '_bw',
+        extname: '.webp',
+    },
+    }, {
+      // original -> webp
+      rename: {
+        suffix: '',
+        extname: '.webp',
+      },
+    }, {
+      // Empty case to produce a copy of the original
+    }],
+  }, {
+    // Global configuration for all images
+    quality: 80,
+    compressionLevel: 7,
+    progressive: true,
+    withMetadata: false,
+    errorOnUnusedConfig: false,
+    skipOnEnlargement: true,
+    errorOnEnlargement: false,
+    silent: true,
+  }))
+  // .pipe(vinylPaths(del))
+  .pipe(gulp.dest("content/images/_working/processed/"));
 }));
 
 gulp.task("upload", gulp.series('img-variants', function (done) {
