b102847ec7a237752d17c2ef2b5a3c2636ef37d3
Jeremy Zilar <jeremy.zilar@gsa.gov>
2019-10-08 22:30:20 -0400

    optimizing

--- config/gulp/images.js
+++ config/gulp/images.js
@@ -35,26 +35,31 @@ var gulp          = require("gulp"),
     git           = require('gulp-git');
 
 
-gulp.task("file-tidy", function (done) {
+// Clean up the filename before processing
+gulp.task("file-tidy", gulp.series(function(done){
   return gulp.src("content/images/_inbox/*.{png,jpg,jpeg}")
-    .pipe(replace(/[ &$_#!?.]/g, '-'))            // special chars to dashes
-    .pipe(replace(/-+/g, '-'))                    // multiple dashes to a single dash
-    .pipe(replace(/-(png|jpg|jpeg)/g, '.$1'))     // remove trailing dashes
-    .pipe(replace(/\.jpeg$/g, '.jpg'))            // .jpeg to .jpg
-    .pipe(replace(/-\d{2,4}x\d{2,4}(?=\.jpg)/g, ''))      // strip trailing dimensions
+    .pipe(replace(/[ &$_#!?.]/g, '-'))                 // special chars to dashes
+    .pipe(replace(/-+/g, '-'))                         // multiple dashes to a single dash
+    .pipe(replace(/-(png|jpg|jpeg)/g, '.$1'))          // remove trailing dashes
+    .pipe(replace(/\.jpeg$/g, '.jpg'))                 // .jpeg to .jpg
+    .pipe(replace(/-\d{2,4}x\d{2,4}(?=\.jpg)/g, ''))   // strip trailing dimensions
     .pipe(replace(/^\d{2,4}-*x-*\d{2,4}-*/g, ''))      // strip leading dimensions
-    .pipe(replace(/-\./g, '.'))                   // remove leading dashes
-    .pipe(replace(/^-/g, ''))                     // removes dashes from the start of filename
-    .pipe(rename(function(path) { // make filename lowercase
+    .pipe(replace(/-\./g, '.'))                        // remove leading dashes
+    .pipe(replace(/^-/g, ''))                          // removes dashes from start of filename
+
+    // make filename lowercase
+    .pipe(rename(function(path) {
       path.basename = changeCase.lowerCase(path.basename);
       path.extname = changeCase.lowerCase(path.extname);
     }))
-    // Updates the original in content/images/_working/
+
+    // copies the original to content/images/_working/
     .pipe(gulp.dest("content/images/_working/originals/"))
     .pipe(gulp.dest("content/images/_working/to-process/"))
-});
+}));
 
-gulp.task("clean-inbox", gulp.series('file-tidy', function(done){
+// removes the original from content/images/_inbox/
+gulp.task("clean-inbox", gulp.series('file-tidy', function(){
   return del(['content/images/_inbox/**', '!content/images/_inbox', '!content/images/_inbox/__add jpg and png files to this folder__']);
 }));
 
@@ -73,47 +78,32 @@ function get_curr_date(){
   return output;
 }
 
-function get_image_uid(path){
-  var uid = /([^\/]+)(?=\.\w+$)/g; // gets the slug/filename from the path
-  return path.match(uid);
-}
-
-function get_image_format(path){
-  var format = path.split('.').pop(); // gets the file extension from the path
-  return format;
-}
-
-function get_image_data(uid, width, height, format){
-  var data = [
-    "date     : " + get_curr_date(),
-    "uid      : " + uid,
-    "width    : " + width,
-    "height   : " + height,
-    "format   : " + format,
-    "credit   : ",
-    "caption  : ",
-    "alt      : "
-  ].join("\n");
-  return data;
-}
-
-function get_image_sq(path){
-  var dimensions = sizeOf(path); // gets the file extension from the path
-  var sq_dim = Math.min(dimensions.width, dimensions.height);
-  return sq_dim;
-}
-
-gulp.task("img-variants", gulp.series('clean-inbox', function (done) {
+gulp.task("write-data-file", gulp.series('clean-inbox', function (done) {
   return gulp.src("content/images/_working/to-process/*.{png,jpg,jpeg,JPG,JPEG,PNG}")
-    // Create responsive variants
+    // write the .yml file for this image
     .pipe(tap(function (file) {
-      var uid = get_image_uid(file.path);
-      var format = get_image_format(file.path);
+      var uid = file.path.match('/([^\/]+)(?=\.\w+$)/g'); // gets the slug/filename from the path
+      var format = file.path.split('.').pop();
       var dimensions = sizeOf(file.path);
-      fs.writeFile('data/images/'+ uid +'.yml', get_image_data(uid, dimensions.width, dimensions.height, format), function(){
+      var img_data = [
+        "date     : " + get_curr_date(),
+        "uid      : " + uid,
+        "width    : " + dimensions.width,
+        "height   : " + dimensions.height,
+        "format   : " + format,
+        "credit   : ",
+        "caption  : ",
+        "alt      : "
+      ].join("\n");
+      fs.writeFile('data/images/'+ uid +'.yml', img_data, function(){
         console.log('image file written');
       });
     }))
+}));
+
+gulp.task("mkdir", gulp.series('write-data-file', function (done) {
+  return gulp.src("content/images/_working/to-process/*.{png,jpg,jpeg,JPG,JPEG,PNG}")
+    // Create the processed folder
     .pipe(tap(function (file) {
       var dir = 'content/images/_working/processed';
       if (!fs.existsSync(dir)){
@@ -122,6 +112,10 @@ gulp.task("img-variants", gulp.series('clean-inbox', function (done) {
         };
       }
     }))
+}));
+
+gulp.task("img-variants", gulp.series('mkdir', function (done) {
+  return gulp.src("content/images/_working/to-process/*.{png,jpg,jpeg,JPG,JPEG,PNG}")
     // Create responsive variants
     .pipe(responsive({
     '*': [{
@@ -338,7 +332,7 @@ gulp.task("img-variants", gulp.series('clean-inbox', function (done) {
   .pipe(gulp.dest("content/images/_working/processed/"));
 }));
 
-gulp.task("proxy", gulp.series('img-variants', function (done) {
+gulp.task("img-proxy", gulp.series('mkdir', function (done) {
   // - - - - - - - - - - - - - - - - -
   // Create lorez version for Hugo to parse
   return gulp.src("content/images/_working/originals/*.{png,jpg}")
@@ -365,7 +359,7 @@ gulp.task("proxy", gulp.series('img-variants', function (done) {
     .pipe(gulp.dest("static/img/proxy/"));
 }));
 
-gulp.task("upload", gulp.series('proxy', function (done) {
+gulp.task("upload", gulp.series(gulp.parallel('img-variants', 'img-proxy'), function (done) {
   return gulp.src("content/images/_working/processed/**/*")
     .pipe(s3({
       Bucket: 'digitalgov',   //  Required
