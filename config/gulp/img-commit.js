0bc44f5c7b1617b4b01f58e5bbf0e2cc7c039c51
Jeremy Zilar <jeremy.zilar@gsa.gov>
2019-10-08 23:33:14 -0400

    changing scope to /data

--- config/gulp/img-commit.js
+++ config/gulp/img-commit.js
@@ -4,13 +4,13 @@ var git           = require('gulp-git');
 
 gulp.task("git-add", gulp.series(function (done) {
   console.log('adding');
-  return gulp.src('./*')
+  return gulp.src('data/*')
     .pipe(git.add());
 }));
 
 gulp.task("git-commit", gulp.series('git-add', function (done) {
   console.log('commiting');
-  return gulp.src('./*')
+  return gulp.src('data/*')
     .pipe(git.commit('adding in images', {args: '-a'}));
 }));
 
