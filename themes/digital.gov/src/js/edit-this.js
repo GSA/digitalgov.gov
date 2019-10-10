ace04109447fa97836620cf6cce2e97fc5c92ae6
Jeremy Zilar <jeremy.zilar@gsa.gov>
2019-10-10 00:19:39 -0400

    adding in the bug issue button

--- themes/digital.gov/src/js/edit-this.js
+++ themes/digital.gov/src/js/edit-this.js
@@ -14,22 +14,21 @@ jQuery(document).ready(function($) {
 		});
 	}
 
-	$('.edit_tools .editor').click(function(e){
+	$('.edit_tools .edit-open').click(function(e){
 		e.preventDefault();
 		// If the editing tool is already active
-		if ( $(this).is( ".active" ) ) {
+		if ( $('.edit_tools').is( ".active" ) ) {
 			// runs a function that removes edit tools from each item on the page that is editable.
 			disable_edit_this();
 			// remove the active class from the edit button
-			$(this).removeClass('active');
+			$('.edit_tools').removeClass('active');
 			// swap out the icon in the edit button
 			$(this).html('<i class="far fa-edit"></i>');
 		} else {
 			enable_edit_this();
-			$(this).addClass('active');
+			$('.edit_tools').addClass('active');
 			$(this).html('<i class="fas fa-times"></i>');
 		}
-		 // <i class="fas fa-times"></i>
   });
 
 });
