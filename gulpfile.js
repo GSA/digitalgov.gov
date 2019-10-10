f6666368a16e5e6e1d8c9d53fa5fac05b9e72757
Jeremy Zilar <jeremy.zilar@gsa.gov>
2019-10-10 00:19:49 -0400

    new packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,12 @@
-require('./config/gulp/img-prep');
-require('./config/gulp/img-process');
-require('./config/gulp/img-upload');
-require('./config/gulp/img-commit');
-require('./config/gulp/scripts');
-require('./config/gulp/fontawesome');
-require('./config/gulp/uswds');
 var gulp = require("gulp");
-
-
-// Image Process tasks
-gulp.task("img-prep", gulp.series('mkdir'));
-gulp.task("img-process", gulp.series(gulp.parallel('img-variants', 'img-proxy')));
-gulp.task("img-upload", gulp.series('cleanup'));
-// gulp.task("img-commit", gulp.series('git-push'));
-
-gulp.task("img", gulp.series('img-upload'));
+// require('./config/gulp/img-prep');
+// require('./config/gulp/img-process');
+// require('./config/gulp/img-upload');
+// require('./config/gulp/img-commit');
+//
+require('./config/gulp/scripts');
+// require('./config/gulp/fontawesome');
+require('./config/gulp/uswds');
 
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -33,20 +25,6 @@ const PROJECT_SASS_SRC  = './themes/digital.gov/src/scss/new';
 const PROJECT_JS_SRC  = './themes/digital.gov/src/js';
 
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// gulp.task('watch', gulp.series('build-sass', function (){
-  // gulp.watch(`${PROJECT_SASS_SRC}/**/*.scss`, gulp.series('build-sass'))
-  // gulp.watch(`${PROJECT_SASS_SRC}/scss/*.scss`, gulp.series('build-sass'))
-  // gulp.watch(`${PROJECT_JS_SRC}/*.js`, gulp.series('compile'))
-  // gulp.watch(`${USWDS_SRC}/scss/*.scss`, gulp.series('copy-uswds-assets','build-sass'));
-// }));
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// Set watch as default task
-// gulp.task('default', gulp.series('watch', 'copy-uswds-assets', 'copy-uswds-fonts', 'build-sass', 'fontawesome', 'compile'));
-// gulp.task('default', gulp.series('watch', 'compile','build-sass'));
-
-
 gulp.task('watch-sass', function () {
   gulp.watch(`${USWDS_SRC}/**/*.scss`, gulp.series('build-sass'));
   gulp.watch(`${PROJECT_SASS_SRC}/**/*.scss`, gulp.series('build-sass'));
@@ -56,3 +34,14 @@ gulp.task('watch-sass', function () {
 gulp.task('watch', gulp.series('build-sass', 'copy-jquery', 'compile', 'watch-sass'));
 
 gulp.task('default', gulp.series('watch'));
+
+
+
+
+// Image Process tasks
+// gulp.task("img-prep", gulp.series('mkdir'));
+// gulp.task("img-process", gulp.series(gulp.parallel('img-variants', 'img-proxy')));
+// gulp.task("img-upload", gulp.series('cleanup'));
+// // gulp.task("img-commit", gulp.series('git-push'));
+//
+// gulp.task("img", gulp.series('img-upload'));
