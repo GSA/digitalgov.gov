4e1a2ce47946b8e242f5c09ceb35d3044645b37e
Jeremy Zilar <jeremy.zilar@gsa.gov>
2020-03-31 18:10:24 -0400

    adding abiilty to compile additional js files separately

--- config/gulp/scripts.js
+++ config/gulp/scripts.js
@@ -3,18 +3,37 @@ var gulp          = require("gulp"),
     concat        = require("gulp-concat"),
     jshint        = require("gulp-jshint")
 
+
+// Javascript destination
+const JS_SRC = './themes/digital.gov/src/js';
+
 // task
-gulp.task('compile', function (done) {
-  return gulp.src('./themes/digital.gov/src/js/**/*.js') // path to your files
+gulp.task('compile-common', function (done) {
+  return gulp.src([
+    `${JS_SRC}/common/**/*.js`
+  ])
+
   .pipe(jshint())
   .pipe(jshint.reporter()) // Dump results
   .pipe(uglify())
-  .pipe(concat('base.js'))
+  .pipe(concat('common.js'))
   .pipe(gulp.dest('./themes/digital.gov/static/dist'));
 });
 
 const JQUERY_PATH = 'node_modules/jquery/dist/jquery.min.js';
 
+// Copy all the JS files that are not common
+gulp.task('compile', function(done) {
+  return gulp.src([
+    `${JS_SRC}/*.js`
+  ])
+  .pipe(jshint())
+  .pipe(jshint.reporter()) // Dump results
+  .pipe(uglify())
+  .pipe(gulp.dest('./themes/digital.gov/static/dist'));
+});
+
+// Copy jQuery
 gulp.task('copy-jquery', () => {
   return gulp.src(`${JQUERY_PATH}`)
   .pipe(gulp.dest('./themes/digital.gov/static/dist'));
