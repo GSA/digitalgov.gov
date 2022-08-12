dafbcd625ca4371f87f0e6c3ca00578a552b3a3c
Nick Lyons <nick.lyons@gsa.gov>
2022-08-12 11:40:38 -0400

    updated gulpfile to process images for 'npm start'

--- gulpfile.js
+++ gulpfile.js
@@ -11,12 +11,16 @@ const fa = require("./config/gulp/fontawesome"),
   scripts = require("./config/gulp/scripts"),
   styles = require("./config/gulp/styles");
 
+function watchImages() {
+  return series(img.prep.do, img.process.do, img.upload.do);
+}
+
 function gulpWatch() {
   const THEME_DIR = "./themes/digital.gov/src";
   watch(`${THEME_DIR}/scss/uswds/**/*.scss`, styles.buildSass);
   watch(`${THEME_DIR}/scss/new/**/*.scss`, styles.buildSass);
   watch(`${THEME_DIR}/js/**/*.js`, scripts.compile);
-  watch("./content/images/_inbox/*.*", img);
+  watch("./content/images/_inbox/*.*", watchImages());
 }
 
 // Define public tasks
