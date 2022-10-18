eca9a75ba3f329e0437dbe2e12fb997764db1fc3
Nick Lyons <nick.lyons@gsa.gov>
2022-10-18 12:32:30 -0400

    added aria labels

--- themes/digital.gov/src/js/common/subscribe.js
+++ themes/digital.gov/src/js/common/subscribe.js
@@ -1,7 +1,8 @@
 // scroll form into view when on /subscribe page
-const subscribeButton = document.querySelector(".subscribe-button")
-const signupForm = document.querySelector(".usa-sign-up")
+const subscribeButton = document.querySelector(".subscribe-button");
+const signupForm = document.querySelector(".usa-sign-up");
 
-subscribeButton.addEventListener('click', function(event) {
-  signupForm.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+subscribeButton.addEventListener("click", function() {
+  signupForm.scrollIntoView({behavior: "smooth", block: "start"})
+  signupForm.focus();
 })
