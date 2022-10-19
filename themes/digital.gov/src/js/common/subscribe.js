a09ae7c87edd8f91bd8ae65b9c4a989b31916724
Nick Lyons <nick.lyons@gsa.gov>
2022-10-19 12:08:05 -0400

    updated js and css suggestions

--- themes/digital.gov/src/js/common/subscribe.js
+++ themes/digital.gov/src/js/common/subscribe.js
@@ -1,6 +1,6 @@
 // scroll form into view when on /subscribe page
 const subscribeButton = document.querySelector(".subscribe-button");
-const signupForm = document.querySelector(".usa-sign-up");
+const signupForm = document.getElementById("#newsletter-signup ");
 
 subscribeButton.addEventListener("click", function() {
   signupForm.scrollIntoView({behavior: "smooth", block: "start"})
