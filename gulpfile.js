065951d6bc37dcde455a4951e2a82d13a2dbb53e
Nick Lyons <nick.lyons@gsa.gov>
2022-11-14 15:11:04 -0500

    removed gulp-responsive and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ const { parallel, series, src, watch } = require("gulp");
 // Import task functions
 const img = {
     prep: require("./config/gulp/img-prep"),
-    process: require("./config/gulp/img-process"),
+    // process: require("./config/gulp/img-process"),
     upload: require("./config/gulp/img-upload"),
   },
   scripts = require("./config/gulp/scripts"),
   styles = require("./config/gulp/styles");
 
 function watchImages() {
-  return series(img.prep.do, img.process.do, img.upload.do);
+  return series(img.prep.do, img.upload.do); //return series(img.prep.do, img.process.do, img.upload.do);
 }
 
 function gulpWatch() {
@@ -29,6 +29,6 @@ exports.copyUswdsFonts = styles.copyUswdsFonts;
 exports.copyUswdsAssets = parallel(styles.copyUswdsImages, styles.copyUswdsJs, styles.copyUswdsFonts);
 exports.buildAssets = parallel(styles.buildSass, scripts.compile);
 exports.buildSass = styles.buildSass;
-exports.img = series(img.prep.do, img.process.do, img.upload.do);
+exports.img = series(img.prep.do, img.upload.do);
 exports.watch = gulpWatch;
 exports.default = series(styles.buildSass, gulpWatch);
