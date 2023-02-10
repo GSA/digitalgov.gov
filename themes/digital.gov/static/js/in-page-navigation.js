c7c0f3d8ead977e1604e0ad460986b0ecbf682fd
Nick Lyons <nick.lyons@gsa.gov>
2023-02-10 13:34:39 -0500

    added missing stylesheets

---
+++ themes/digital.gov/static/js/in-page-navigation.js
@@ -0,0 +1,6 @@
+let relatedItems = document.querySelector(".content__related-items");
+let inPageNav = document.querySelector(".usa-in-page-nav__nav");
+
+if (relatedItems) {
+  relatedItems.before(inPageNav);
+}
