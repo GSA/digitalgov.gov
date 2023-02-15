f775a6e169af3edf3f6cdfd5db53e0fd243649f8
Nick Lyons <nick.lyons@gsa.gov>
2023-02-15 14:04:11 -0500

    more cleanup

--- themes/digital.gov/src/js/common/in-page-navigation.js
+++ themes/digital.gov/src/js/common/in-page-navigation.js
@@ -3,14 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const inPageNav = document.querySelector(".usa-in-page-nav__nav");
   const navItems = document.querySelectorAll("li.usa-in-page-nav__item:not(.usa-in-page-nav__item--sub-item)");
 
-  console.log(navItems.length);
-
   if (!relatedItems) {
     return;
   }
 
   if (navItems.length < 3) {
-    inPageNav.remove();
+    inPageNav.style.display = none;
     return;
   }
 
