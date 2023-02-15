a8038c82eaa88c4e4e5364bd4a7fa18fdbb18ca8
Nick Lyons <nick.lyons@gsa.gov>
2023-02-15 10:16:28 -0500

    cleaned up markup and styles, refactored javascript for in-page nav

--- themes/digital.gov/src/js/common/in-page-navigation.js
+++ themes/digital.gov/src/js/common/in-page-navigation.js
@@ -1,8 +1,18 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  let relatedItems = document.querySelector(".content__related-items");
-  let inPageNav = document.querySelector(".usa-in-page-nav__nav");
+document.addEventListener("DOMContentLoaded", () => {
+  const relatedItems = document.querySelector(".content__related-items");
+  const inPageNav = document.querySelector(".usa-in-page-nav__nav");
+  const navItems = document.querySelectorAll("li.usa-in-page-nav__item:not(.usa-in-page-nav__item--sub-item)");
 
-  if (relatedItems) {
-    relatedItems.before(inPageNav);
+  console.log(navItems.length);
+
+  if (!relatedItems) {
+    return;
   }
+
+  if (navItems.length < 3) {
+    inPageNav.remove();
+    return;
+  }
+
+  relatedItems.before(inPageNav);
 });
