9f6474abebeafda778636237f822df8800425827
Nick Lyons <nick.lyons@gsa.gov>
2023-02-23 18:37:20 -0500

    fixed display style and h3 to match aside hierarchy

--- themes/digital.gov/src/js/common/in-page-navigation.js
+++ themes/digital.gov/src/js/common/in-page-navigation.js
@@ -3,12 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const inPageNav = document.querySelector(".usa-in-page-nav__nav");
   const navItems = document.querySelectorAll("li.usa-in-page-nav__item:not(.usa-in-page-nav__item--sub-item)");
 
+  console.log(navItems);
   if (!relatedItems) {
     return;
   }
 
   if (navItems.length < 3) {
-    inPageNav.style.display = none;
+    inPageNav.style.display = "none";
     return;
   }
 
