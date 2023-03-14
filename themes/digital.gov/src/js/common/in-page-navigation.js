7ac84c0917f7c462a52059362114769e7098125a
Nick Lyons <nick.lyons@gsa.gov>
2023-03-14 17:18:09 -0400

    replaced hugo toc with in-page nav component

--- themes/digital.gov/src/js/common/in-page-navigation.js
+++ themes/digital.gov/src/js/common/in-page-navigation.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const relatedItems = document.querySelector(".content__related-items");
   const inPageNav = document.querySelector(".usa-in-page-nav__nav");
-  const inPageNavHeader = inPageNav.querySelector(".usa-in-page-nav__heading");
+  const inPageNavHeader = document.querySelector(".usa-in-page-nav__heading");
   const navItems = document.querySelectorAll("li.usa-in-page-nav__item:not(.usa-in-page-nav__item--sub-item)");
 
-
-
-  if (!relatedItems) {
-    return;
-  }
+  console.log(`length: ${navItems.length}`);
 
   if (navItems.length < 3) {
     inPageNav.style.display = "none";
+    return;
+  }
+
+  if (!relatedItems) {
     return;
   }
 
