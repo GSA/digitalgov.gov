4b7605ea4adb3882541a9128ae893a14e19f4c57
Nick Lyons <nick.lyons@gsa.gov>
2023-03-20 15:30:14 -0400

    added check for 404 pages

--- themes/digital.gov/src/js/common/in-page-navigation.js
+++ themes/digital.gov/src/js/common/in-page-navigation.js
@@ -3,6 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const inPageNav = document.querySelector(".usa-in-page-nav__nav");
   const inPageNavHeader = document.querySelector(".usa-in-page-nav__heading");
   const navItems = document.querySelectorAll("li.usa-in-page-nav__item:not(.usa-in-page-nav__item--sub-item)");
+  const errorPage = document.querySelector("h1");
+
+  console.log(errorPage);
+
+  if (errorPage.includes("404 Error")) {
+    console.log("Error Page");
+    inPageNav.style.display = "none";
+    return;
+  }
 
   if (navItems.length < 3) {
     inPageNav.style.display = "none";
