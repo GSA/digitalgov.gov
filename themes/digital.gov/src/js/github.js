bffb13f62659814bbfed4af44282df8debaaf280
James Mejia <james.mejia@gsa.gov>
2023-05-12 14:12:50 -0500

    Fix formatting issues in github.js
    Add eslint-disable to high LOE tasks and create additional cards.

--- themes/digital.gov/src/js/github.js
+++ themes/digital.gov/src/js/github.js
@@ -1,65 +1,15 @@
-jQuery(function ($) {
-  const editFile = `.edit-file`;
-  // Builds the Edit link on posts/pages/events to point to the GitHub file
-  function build_edit_file_link() {
-    // editpathURL is set the <head>
-    if (editpathURL !== undefined) {
-      // Build the edit link
-      const edit = `<a target='_blank' class='edit_file_link' href='${editpathURL}' title='Edit in GitHub'>Edit</a>`;
+// Disabling no-undef because some vars are set in templates.
+// @TODO: Remove var setting in Hugo templates.
+/* eslint-disable no-undef */
 
-      // Insert the .edit-file-link html into the .edit-file div and remove the .hidden class
-      $(`#page-data ${editFile}`).html(edit).removeClass("hidden");
-    }
-  }
-  build_edit_file_link();
+function getBranchLink(branch) {
+  const path = `https://github.com/GSA/digitalgov.gov/tree/${branch}`;
+  const branchLink = `<a class="branch" href="${path}" title="${branch}">${branch}</a>`;
+  return branchLink;
+}
 
-  function get_commit_data() {
-    if (branch == "main") {
-      branchpath = "";
-    } else {
-      branchpath = `/${branch}`;
-    }
-    const commit_api_path = `https://api.github.com/repos/${git_org}/${git_repo}/commits${branchpath}?path=/content/${filepath}`;
-    if (commit_api_path !== undefined) {
-      $.ajax({
-        url: commit_api_path,
-        dataType: "json",
-      }).done(function (data) {
-        if (typeof data !== "undefined") {
-          show_last_commit(data, branch);
-        }
-      });
-    }
-  }
-  get_commit_data(filepath);
-
-  function get_branch_link(branch) {
-    const path = `https://github.com/GSA/digitalgov.gov/tree/${branch}`;
-    const branch_link = `<a class="branch" href="${path}" title="${branch}">${branch}</a>`;
-    return branch_link;
-  }
-
-  function show_last_commit(data, branch) {
-    const branch_link = get_branch_link(branch);
-    const commit_data = Array.isArray(data) ? data[0] : data;
-    const commit_date = commit_data.commit.committer.date;
-    const commit_history_url = `https://github.com/GSA/digitalgov.gov/commits/${branch}/content/${filepath}`;
-
-    last_commit = [
-      branch_link,
-      `<p>Last updated on <a href=${commit_history_url}>
-        <span class='commit-date'>${formatDate(commit_date)}</span>
-      </a></p>`,
-    ];
-
-    $(`${editFile}`).each(function (i, items_list) {
-      $(this).append(last_commit.join("\n"));
-    });
-  }
-});
-
-function formatDate(timezone_date) {
-  const input_date = new Date(timezone_date);
+function formatDate(timezoneDate) {
+  const inputDate = new Date(timezoneDate);
 
   const dateOptions = {
     day: "numeric",
@@ -74,10 +24,66 @@ function formatDate(timezone_date) {
     timeZoneName: "shortGeneric",
   };
 
-  const output_date = input_date.toLocaleDateString(undefined, dateOptions);
-  const output_time = input_date
+  const outputDate = inputDate.toLocaleDateString(undefined, dateOptions);
+  const outputTime = inputDate
     .toLocaleTimeString("en-US", timeOptions)
     .replace("AM", "a.m.,")
     .replace("PM", "p.m.,");
-  return `${output_date} at ${output_time}`;
+  return `${outputDate} at ${outputTime}`;
 }
+
+function showLastCommit(data, branch) {
+  const branchLink = getBranchLink(branch);
+  const commitData = Array.isArray(data) ? data[0] : data;
+  const commitDate = commitData.commit.committer.date;
+  // eslint-disable-next-line no-undef
+  const commitHistoryUrl = `https://github.com/GSA/digitalgov.gov/commits/${branch}/content/${filepath}`;
+
+  const lastCommit = [
+    branchLink,
+    `<p>Last updated on <a href=${commitHistoryUrl}>
+        <span class='commit-date'>${formatDate(commitDate)}</span>
+      </a></p>`,
+  ];
+
+  // eslint-disable-next-line no-unused-vars, func-names
+  $(".edit_file").each(function (i, itemsList) {
+    $(this).append(lastCommit.join("\n"));
+  });
+}
+
+jQuery(($) => {
+  function buildEditFileLink() {
+    // editpathURL is set the <head>
+    if (editpathURL !== undefined) {
+      // Build the edit link
+      const edit = `<a target='_blank' class='edit_file_link' href='${editpathURL}' title='Edit in GitHub'>Edit</a>`;
+
+      // Insert the .edit_file_link html into the .edit_file div and remove the .hidden class
+      $("#page-data .edit_file").html(edit).removeClass("hidden");
+    }
+  }
+  buildEditFileLink();
+
+  function getCommitData() {
+    if (branch === "main") {
+      branchpath = "";
+    } else {
+      branchpath = `/${branch}`;
+    }
+    // eslint-disable-next-line camelcase
+    const commitApiPath = `https://api.github.com/repos/${git_org}/${git_repo}/commits${branchpath}?path=/content/${filepath}`;
+
+    if (commitApiPath !== undefined) {
+      $.ajax({
+        url: commitApiPath,
+        dataType: "json",
+      }).done((data) => {
+        if (typeof data !== "undefined") {
+          showLastCommit(data, branch);
+        }
+      });
+    }
+  }
+  getCommitData(filepath);
+});
