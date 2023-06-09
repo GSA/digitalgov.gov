01d3bf51f4f98c3f5071539594682ced1f209a44
Nick Lyons <nick.lyons@gsa.gov>
2023-06-09 13:57:41 -0400

    Fixed javascript formatting

--- themes/digital.gov/src/js/guide-toc.js
+++ themes/digital.gov/src/js/guide-toc.js
@@ -2,19 +2,19 @@
 // Replaces guidenav.js that used Hugo's .TableOfContents with Summary Box component — https://designsystem.digital.gov/components/summary-box/#package
 // Get all H2's and create a top of the page Table of Contents if shortcode is used on the markdown page
 
-const guideTOCList = document.querySelector(".usa-list");
 const guideTOC = document.querySelector(".usa-summary-box");
+const guideTOCList = document.querySelector(".usa-list");
 
 if (guideTOC) {
-  let guideLinks = document.querySelectorAll("h2");
-  let linksToDisplay = [];
-  guideLinks.forEach(function (link) {
+  const guideLinks = document.querySelectorAll("h2");
+  const linksToDisplay = [];
+  guideLinks.forEach((link) => {
     linksToDisplay.push(
       `<li><a class="usa-summary-box__link" href="#${link.id}">${link.innerText}</a></li>`
     );
   });
 
-  linksToDisplay.forEach(function (link) {
+  linksToDisplay.forEach((link) => {
     guideTOCList.insertAdjacentHTML("beforeend", link);
   });
 }
