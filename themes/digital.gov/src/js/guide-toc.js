9370c113a93c41882e84bafdf75d59f1c72d900c
Nick Lyons <nick.lyons@gsa.gov>
2023-06-21 14:37:01 -0400

    Added guide chapter style progress indicator, added .current to li, wip

--- themes/digital.gov/src/js/guide-toc.js
+++ themes/digital.gov/src/js/guide-toc.js
@@ -5,6 +5,14 @@
 const guideTOC = document.querySelector(".usa-summary-box");
 const guideTOCList = document.querySelector(".usa-list");
 
+const topListItem = document.querySelector(".usa-sidenav .usa-current");
+const parent = topListItem.parentNode;
+parent.classList.add("current");
+// console.log(topListItem, parent);
+
+// .parents("li").last();
+// topListItem.addClass("current");
+
 if (guideTOC) {
   const guideLinks = document.querySelectorAll("h2");
   const linksToDisplay = [];
@@ -16,5 +24,26 @@ if (guideTOC) {
 
   linksToDisplay.forEach((link) => {
     guideTOCList.insertAdjacentHTML("beforeend", link);
+  });
+}
+
+// Progressively style each guide chapter for the reader
+
+const guideLinks = Array.from(document.querySelectorAll(".usa-sidenav__item"));
+console.log(guideLinks);
+
+guideLinks.forEach(function (link, index) {
+  if (link.querySelector(".usa-current")) {
+    updateGuideLinks(index);
+  }
+});
+
+function updateGuideLinks(linkPosition) {
+  guideLinks.forEach(function (link, index) {
+    if (index < linkPosition) {
+      link.classList.add("usa-sidenav__item--read");
+    } else {
+      link.classList.remove("usa-sidenav__item--read");
+    }
   });
 }
