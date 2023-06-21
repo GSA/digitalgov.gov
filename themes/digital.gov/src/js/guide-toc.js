a5f3667729a5393346bdc91995468f7f68edce4a
Nick Lyons <nick.lyons@gsa.gov>
2023-06-21 15:22:33 -0400

    Fixed formatting and js errors

--- themes/digital.gov/src/js/guide-toc.js
+++ themes/digital.gov/src/js/guide-toc.js
@@ -1,17 +1,11 @@
 // Guide Table of Contents
 // Replaces guidenav.js that used Hugo's .TableOfContents with Summary Box component — https://designsystem.digital.gov/components/summary-box/#package
 // Get all H2's and create a top of the page Table of Contents if shortcode is used on the markdown page
-
 const guideTOC = document.querySelector(".usa-summary-box");
 const guideTOCList = document.querySelector(".usa-list");
 
 const topListItem = document.querySelector(".usa-sidenav .usa-current");
-const parent = topListItem.parentNode;
-parent.classList.add("current");
-// console.log(topListItem, parent);
-
-// .parents("li").last();
-// topListItem.addClass("current");
+topListItem.parentNode.classList.add("current");
 
 if (guideTOC) {
   const guideLinks = document.querySelectorAll("h2");
@@ -27,19 +21,10 @@ if (guideTOC) {
   });
 }
 
-// Progressively style each guide chapter for the reader
-
+// Progressively style the previous guide list item to indicate visual progress
 const guideLinks = Array.from(document.querySelectorAll(".usa-sidenav__item"));
-console.log(guideLinks);
-
-guideLinks.forEach(function (link, index) {
-  if (link.querySelector(".usa-current")) {
-    updateGuideLinks(index);
-  }
-});
-
 function updateGuideLinks(linkPosition) {
-  guideLinks.forEach(function (link, index) {
+  guideLinks.forEach((link, index) => {
     if (index < linkPosition) {
       link.classList.add("usa-sidenav__item--read");
     } else {
@@ -47,3 +32,9 @@ function updateGuideLinks(linkPosition) {
     }
   });
 }
+
+guideLinks.forEach((link, index) => {
+  if (link.querySelector(".usa-current")) {
+    updateGuideLinks(index);
+  }
+});
