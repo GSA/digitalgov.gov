6ed94ec7889727691767dc5647824c5b5a2b78bf
Jackson Moody <jackson.moody@gsa.gov>
2023-07-10 13:47:49 -0500

    Scroll menu bar into view

---
+++ themes/digital.gov/src/js/guide-menu-bar.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const currentItem = document.querySelector(".menu-bar-guide #current");
+  if (!currentItem) return;
+  currentItem.scrollIntoView({ block: 'nearest', inline: 'center' })
+});
