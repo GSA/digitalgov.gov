a1d824a46568f01c1aedaf26cb78fccf5305f88b
Jackson Moody <jackson.moody@gsa.gov>
2023-07-10 19:05:36 -0500

    Ran prettier formatting

--- themes/digital.gov/src/js/guide-menu-bar.js
+++ themes/digital.gov/src/js/guide-menu-bar.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const currentItem = document.querySelector(".menu-bar-guide #current");
   if (!currentItem) return;
-  currentItem.scrollIntoView({ block: 'nearest', inline: 'center' })
+  currentItem.scrollIntoView({ block: "nearest", inline: "center" });
 });
