0a48670621b3cfdbb1969254df2fc83b06192df4
Jackson Moody <jackson.moody@gsa.gov>
2023-07-18 18:15:02 -0600

    Added logo (conditionally) and removed scroll

--- themes/digital.gov/src/js/guide-menu-bar.js
+++ themes/digital.gov/src/js/guide-menu-bar.js
@@ -3,3 +3,22 @@ document.addEventListener("DOMContentLoaded", () => {
   if (!currentItem) return;
   currentItem.scrollIntoView({ block: "nearest", inline: "center" });
 });
+
+const menuBar = document.querySelector(".dg-guide__menu-bar");
+const menuBarImage = document.querySelector(".dg-guide__menu-bar-image");
+
+function intersection(e) {
+  if (!e.isIntersecting && e.boundingClientRect.top < 100) {
+    menuBarImage.style.visibility = "visible";
+  } else {
+    menuBarImage.style.visibility = "hidden";
+  }
+  menuBarImage.classList.toggle('isSticky');
+}
+
+const observer = new IntersectionObserver( 
+  ([e]) => intersection(e),
+  {threshold: [1]}
+);
+
+observer.observe(menuBar);
