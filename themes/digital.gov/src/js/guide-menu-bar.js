eb297753ae3473b7c77254372687a174635a7e04
Jackson Moody <jackson.moody@gsa.gov>
2023-07-19 14:40:29 -0600

    Updated scroll behavior

--- themes/digital.gov/src/js/guide-menu-bar.js
+++ themes/digital.gov/src/js/guide-menu-bar.js
@@ -1,23 +1,51 @@
+const menuBarImageWidth = 150;
+const menuBarImageWidthMobile = 30;
+const menuBarScrollOffset = 40;
+const deviceBreakpoint = 480;
+
 const menuBar = document.querySelector(".dg-guide__menu-bar");
 const menuBarImageContainer = document.querySelector(
   ".dg-guide__menu-bar-image"
 );
 const menuBarLinks = document.querySelector(".dg-guide__menu-bar-links");
-const menuBarImage = document.createElement("img");
-const menuBarImageWidth = 150;
-menuBarImage.src = "/img/digitalgov-logo.svg";
-menuBarImage.alt = "Digital Gov Logo";
-menuBarImage.style.maxWidth = menuBarImageWidth + "px";
-menuBarImage.style.minWidth = menuBarImageWidth + "px";
+const menuBarImage = Object.assign(document.createElement("img"), {
+  src: "/img/digitalgov-logo.svg",
+  alt: "Digital Gov Logo",
+});
+
+Object.assign(menuBarImage.style, {
+  maxWidth: menuBarImageWidth + "px",
+  minWidth:  menuBarImageWidth + "px",
+  marginLeft: "1em",
+  marginRight: "1em"
+});
+
+const menuBarImageMobile = Object.assign(document.createElement("img"), {
+  src: "/img/digit-100.svg",
+  alt: "Digital Gov Logo Small",
+});
+
+Object.assign(menuBarImageMobile.style, {
+  maxWidth: menuBarImageWidthMobile + "px",
+  minWidth:  menuBarImageWidthMobile + "px",
+  marginLeft: "1em",
+  marginRight: "1em"
+});
 
 function intersection(e) {
   if (!e.isIntersecting && e.boundingClientRect.top < 100) {
-    menuBarImageContainer.appendChild(menuBarImage);
+    console.log(window.innerWidth);
+    if (window.innerWidth < deviceBreakpoint) {
+      menuBarImageContainer.appendChild(menuBarImageMobile);
+    } else {
+      menuBarImageContainer.appendChild(menuBarImage);
+    }
     menuBar.style.justifyContent = "space-evenly";
     scrollMenuBar(true);
   } else {
     menuBar.style.justifyContent = "center";
     menuBarImage.remove();
+    menuBarImageMobile.remove();
     scrollMenuBar(false);
   }
 }
@@ -32,9 +60,13 @@ function scrollMenuBar(intersect) {
   const currentItem = document.querySelector(".dg-guide__menu-bar #current");
   if (!currentItem || !menuBarLinks) return;
   if (intersect) {
-    menuBarLinks.scrollLeft = currentItem.offsetLeft - menuBarImageWidth - 100;
+    if (window.innerWidth < deviceBreakpoint) {
+      menuBarLinks.scrollLeft = currentItem.offsetLeft - menuBarImageWidthMobile - menuBarScrollOffset;
+    } else {
+      menuBarLinks.scrollLeft = currentItem.offsetLeft - menuBarImageWidth - menuBarScrollOffset;
+    }
   } else {
-    menuBarLinks.scrollLeft = currentItem.offsetLeft - 100;
+    menuBarLinks.scrollLeft = currentItem.offsetLeft - menuBarScrollOffset;
   }
 }
 
