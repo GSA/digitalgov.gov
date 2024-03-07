6b3b25b7b6e258dc0fd11f0de63b57aa9949f5e7
Nick Lyons <nick.lyons@gsa.gov>
2024-03-07 14:13:09 -0500

    Added jsdocs to document code

--- themes/digital.gov/src/js/summary-box.js
+++ themes/digital.gov/src/js/summary-box.js
@@ -1,7 +1,8 @@
-// Guide Table of Contents
-// Replaces guidenav.js that used Hugo's .TableOfContents with Summary Box component — https://designsystem.digital.gov/components/summary-box/#package
-// Get all H2's and create a top of the page Table of Contents if shortcode is used on the markdown page
-
+/**
+ * Guide Table of Contents
+ * Replaces guidenav.js that used Hugo's .TableOfContents with USWDS summary-box — https://designsystem.digital.gov/components/summary-box/#package
+ * Get all H2's and create a top of the page Table of Contents if shortcode is used on the markdown page
+ */
 // eslint-disable-next-line func-names
 (function () {
   const guideSummary = document.querySelector(".dg-guide-summary");
@@ -9,10 +10,17 @@
   if (!guideSummary) return;
 
   const guideSummaryList = guideSummary.querySelector(".usa-list");
+  /**
+   * Return a list of h2's to display in the summary box
+   * and filter out h2's we do not want to show with the :not selector
+   */
   const pageHeaders = document.querySelectorAll(
     "h2:not(.usa-summary-box__heading, .dg-guide__content-header-title, .dg-glossary__header)"
   );
 
+  /**
+   * Populate the summary box with bulleted list of links created from the current page's h2's
+   */
   function createSummaryBox() {
     const summaryBoxFragment = document.createDocumentFragment();
     pageHeaders.forEach((link) => {
