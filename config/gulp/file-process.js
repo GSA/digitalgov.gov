4afc5b2c858894fbe6f4265421fd1023976cee0f
Nick Lyons <nick.lyons@gsa.gov>
2024-04-02 16:32:50 -0400

    Removed un-used image variants

--- config/gulp/file-process.js
+++ config/gulp/file-process.js
@@ -25,37 +25,12 @@ function variants() {
               },
               {
                 width: 200,
-                skipOnEnlargement: false,
-                rename: {
-                  suffix: "_bu",
-                  extname: ".jpg",
-                },
-                quality: 20,
-                blur: true,
-              },
-              {
-                width: 200,
                 rename: {
                   suffix: "_w200",
                   extname: ".webp",
                 },
               },
               {
-                width: 200,
-                grayscale: true,
-                rename: {
-                  suffix: "_w200bw",
-                },
-              },
-              {
-                width: 200,
-                grayscale: true,
-                rename: {
-                  suffix: "_w200bw",
-                  extname: ".webp",
-                },
-              },
-              {
                 width: 400,
                 rename: {
                   suffix: "_w400",
@@ -65,21 +40,6 @@ function variants() {
                 width: 400,
                 rename: {
                   suffix: "_w400",
-                  extname: ".webp",
-                },
-              },
-              {
-                width: 400,
-                grayscale: true,
-                rename: {
-                  suffix: "_w400bw",
-                },
-              },
-              {
-                width: 400,
-                grayscale: true,
-                rename: {
-                  suffix: "_w400bw",
                   extname: ".webp",
                 },
               },
@@ -97,21 +57,6 @@ function variants() {
                 },
               },
               {
-                width: 600,
-                grayscale: true,
-                rename: {
-                  suffix: "_w600bw",
-                },
-              },
-              {
-                width: 600,
-                grayscale: true,
-                rename: {
-                  suffix: "_w600bw",
-                  extname: ".webp",
-                },
-              },
-              {
                 width: 800,
                 rename: {
                   suffix: "_w800",
@@ -121,127 +66,6 @@ function variants() {
                 width: 800,
                 rename: {
                   suffix: "_w800",
-                  extname: ".webp",
-                },
-              },
-              {
-                width: 800,
-                grayscale: true,
-                rename: {
-                  suffix: "_w800bw",
-                },
-              },
-              {
-                width: 800,
-                grayscale: true,
-                rename: {
-                  suffix: "_w800bw",
-                  extname: ".webp",
-                },
-              },
-              {
-                width: 1200,
-                rename: {
-                  suffix: "_w1200",
-                },
-              },
-              {
-                width: 1200,
-                rename: {
-                  suffix: "_w1200",
-                  extname: ".webp",
-                },
-              },
-              {
-                width: 1200,
-                grayscale: true,
-                rename: {
-                  suffix: "_w1200bw",
-                },
-              },
-              {
-                width: 1200,
-                grayscale: true,
-                rename: {
-                  suffix: "_w1200bw",
-                  extname: ".webp",
-                },
-              },
-              {
-                width: 1600,
-                rename: {
-                  suffix: "_w1600",
-                },
-              },
-              {
-                width: 1600,
-                rename: {
-                  suffix: "_w1600",
-                  extname: ".webp",
-                },
-              },
-              {
-                width: 1600,
-                grayscale: true,
-                rename: {
-                  suffix: "_w1600bw",
-                },
-              },
-              {
-                width: 1600,
-                grayscale: true,
-                rename: {
-                  suffix: "_w1600bw",
-                  extname: ".webp",
-                },
-              },
-              {
-                width: 2400,
-                rename: {
-                  suffix: "_w2400",
-                },
-              },
-              {
-                width: 2400,
-                rename: {
-                  suffix: "_w2400",
-                  extname: ".webp",
-                },
-              },
-              {
-                width: 2400,
-                grayscale: true,
-                rename: {
-                  suffix: "_w2400bw",
-                },
-              },
-              {
-                width: 2400,
-                grayscale: true,
-                rename: {
-                  suffix: "_w2400bw",
-                  extname: ".webp",
-                },
-              },
-              {
-                // original -> grayscale
-                grayscale: true,
-                rename: {
-                  suffix: "_bw",
-                },
-              },
-              {
-                // original -> grayscale webp
-                grayscale: true,
-                rename: {
-                  suffix: "_bw",
-                  extname: ".webp",
-                },
-              },
-              {
-                // original -> webp
-                rename: {
-                  suffix: "",
                   extname: ".webp",
                 },
               },
@@ -272,9 +96,7 @@ function variants() {
  */
 function removeProcessedImage() {
   console.log("Removing processed images");
-  return del([
-    `content/uploads/_working-images/to-process/*}`,
-  ]);
+  return del([`content/uploads/_working-images/to-process/*}`]);
 }
 
 exports.do = parallel(variants, removeProcessedImage);
