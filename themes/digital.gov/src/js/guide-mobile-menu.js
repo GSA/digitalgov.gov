943ada525ccb9ac48d3eed34add618784bba01c3
Nick Lyons <nick.lyons@gsa.gov>
2024-04-17 14:55:31 -0400

    Updated mobile guide menu UX and styles
    - [x] added color to button
    - [x] reversed button and icons in header
    - [x] removed nav items line, added equal spacing

--- themes/digital.gov/src/js/guide-mobile-menu.js
+++ themes/digital.gov/src/js/guide-mobile-menu.js
@@ -1,10 +1,9 @@
-const deviceBreakpoint = 480;
 const dgGuideMenuBar = document.querySelector(".dg-guide__menu-bar");
 const dgAccordionMobileMenu = document.querySelector(
   ".dg-accordion-mobile-menu"
 );
 const dgAccordionHeader = document.querySelector(
-  ".dg-accordion-mobile-menu--heading"
+  ".dg-accordion-mobile-menu__heading"
 );
 const dgAccordionButton = document.querySelector(
   ".dg-accordion-mobile-menu .usa-accordion__button"
@@ -13,8 +12,25 @@ const dgAccordionContent = document.querySelector(
   ".dg-accordion-mobile-menu .usa-accordion__content"
 );
 
+const desktopBreakpoint = window.matchMedia("(min-width: 480px)");
+
 /**
- * Update usa-accordion state to hidden on page load
+ *  Reset menu based on screen size
+ *
+ * @param {MediaQueryList} breakpoint
+ */
+function handleResize(breakpoint) {
+  if (breakpoint.matches) {
+    dgAccordionMobileMenu.style.display = "none";
+    dgGuideMenuBar.style.display = "flex";
+  } else {
+    dgAccordionMobileMenu.style.display = "block";
+    dgGuideMenuBar.style.display = "none";
+  }
+}
+
+/**
+ * Update usa-accordion state to collapsed on page load
  */
 document.addEventListener("DOMContentLoaded", () => {
   dgAccordionButton.setAttribute("aria-expanded", false);
@@ -22,24 +38,17 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 /**
- * Toggle display of desktop and mobile menus
- */
-if (window.innerWidth < deviceBreakpoint) {
-  dgAccordionMobileMenu.style.display = "block";
-  dgGuideMenuBar.style.display = "none";
-} else {
-  dgAccordionMobileMenu.style.display = "none";
-  dgGuideMenuBar.style.display = "flex";
-}
-/**
- * Toggle the background color on the graphics header
+ * Toggle the background color on the graphics header to match the accordion header
+ * and style the sections button with token blue-warm-20v
  * @param {object} event
  */
 function handleMouseEvent(event) {
   if (event.type === "mouseenter") {
     dgAccordionHeader.style.backgroundColor = "#dfe1e2";
+    dgAccordionButton.style.backgroundColor = "#adcdff";
   } else if (event.type === "mouseleave") {
     dgAccordionHeader.style.backgroundColor = "#edeff0";
+    dgAccordionButton.style.backgroundColor = "#adcdff";
   }
 }
 
@@ -52,3 +61,5 @@ function toggleBackgroundColor() {
 }
 
 toggleBackgroundColor();
+handleResize(desktopBreakpoint);
+desktopBreakpoint.addEventListener("change", handleResize);
