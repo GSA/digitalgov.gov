241c227638e47aca56a773cb6fd40313d2a0a8ea
Nick Lyons <nick.lyons@gsa.gov>
2024-04-30 09:50:17 -0400

    Improve code and ux for mobile menu
    - [x] links go to top of content instead of top of page
    - [x] button is on right side closer to thumb
    - [x] reduced javascript code
    - [x] moved menu links into separate partial
    - [x] simplified styles

--- themes/digital.gov/src/js/guide-mobile-menu.js
+++ themes/digital.gov/src/js/guide-mobile-menu.js
@@ -1,65 +1,43 @@
-const dgGuideMenuBar = document.querySelector(".dg-guide__menu-bar");
-const dgAccordionMobileMenu = document.querySelector(
-  ".dg-accordion-mobile-menu"
-);
-const dgAccordionHeader = document.querySelector(
-  ".dg-accordion-mobile-menu__heading"
-);
-const dgAccordionButton = document.querySelector(
-  ".dg-accordion-mobile-menu .usa-accordion__button"
-);
-const dgAccordionContent = document.querySelector(
-  ".dg-accordion-mobile-menu .usa-accordion__content"
-);
+/**
+ * Display horizontal desktop or vertical mobile menu when viewport size changes
+ *
+ */
 
+const dgGuideMenuBar = document.querySelector(".dg-guide__menu-bar");
+const dgAccordionMobileMenu = document.querySelector(".guide-mobile-menu");
+const dgAccordionContent = document.querySelector(
+  ".guide-mobile-menu .usa-accordion__content"
+);
 const desktopBreakpoint = window.matchMedia("(min-width: 480px)");
 
 /**
  *  Reset menu based on screen size
  *
- * @param {MediaQueryList} breakpoint
+ *  @param {MediaQueryList} breakpoint
  */
 function handleResize(breakpoint) {
   if (breakpoint.matches) {
-    dgAccordionMobileMenu.style.display = "none";
-    dgGuideMenuBar.style.display = "flex";
+    dgAccordionMobileMenu.classList.add("guide-mobile-menu--hide");
+    dgGuideMenuBar.classList.remove("dg-guide__menu-bar--hide");
   } else {
-    dgAccordionMobileMenu.style.display = "block";
-    dgGuideMenuBar.style.display = "none";
+    dgAccordionMobileMenu.classList.remove("guide-mobile-menu--hide");
+    dgGuideMenuBar.classList.add("dg-guide__menu-bar--hide");
   }
 }
 
 /**
- * Update usa-accordion state to collapsed on page load
+ * Update usa-accordion content to hidden on page load
  */
 document.addEventListener("DOMContentLoaded", () => {
-  dgAccordionButton.setAttribute("aria-expanded", false);
   dgAccordionContent.setAttribute("hidden", true);
 });
 
 /**
- * Toggle the background color on the graphics header to match the accordion header
- * and style the sections button with token blue-warm-20v
- * @param {object} event
+ * Initialize guide functions for toggling guide menu
  */
-function handleMouseEvent(event) {
-  if (event.type === "mouseenter") {
-    dgAccordionHeader.style.backgroundColor = "#dfe1e2";
-    dgAccordionButton.style.backgroundColor = "#adcdff";
-  } else if (event.type === "mouseleave") {
-    dgAccordionHeader.style.backgroundColor = "#edeff0";
-    dgAccordionButton.style.backgroundColor = "#adcdff";
-  }
+function initGuideMobileMenu() {
+  handleResize(desktopBreakpoint);
+  desktopBreakpoint.addEventListener("change", handleResize);
 }
 
-/**
- * Listen for mouse enter/leave on accordion button to toggle background color to match for the icon
- */
-function toggleBackgroundColor() {
-  dgAccordionButton.addEventListener("mouseenter", handleMouseEvent);
-  dgAccordionButton.addEventListener("mouseleave", handleMouseEvent);
-}
-
-toggleBackgroundColor();
-handleResize(desktopBreakpoint);
-desktopBreakpoint.addEventListener("change", handleResize);
+initGuideMobileMenu();
