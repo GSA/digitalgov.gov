d6e75770f4a0c3d016c13e2668308970e373210b
Nick Lyons <nick.lyons@gsa.gov>
2024-05-16 15:21:10 -0400

    Resolved second peer review comments

--- themes/digital.gov/src/js/guide-mobile-menu.js
+++ themes/digital.gov/src/js/guide-mobile-menu.js
@@ -1,43 +1,47 @@
 /**
  * Display horizontal desktop or vertical mobile menu when viewport size changes
- *
  */
 
-const dgGuideMenuBar = document.querySelector(".dg-guide__menu-bar");
-const dgAccordionMobileMenu = document.querySelector(".guide-mobile-menu");
-const dgAccordionContent = document.querySelector(
-  ".guide-mobile-menu .usa-accordion__content"
-);
-const desktopBreakpoint = window.matchMedia("(min-width: 480px)");
+// eslint-disable-next-line func-names
+(function () {
+  const dgGuideMenuBar = document.querySelector(".dg-guide__menu-bar");
 
-/**
- *  Reset menu based on screen size
- *
- *  @param {MediaQueryList} breakpoint
- */
-function handleResize(breakpoint) {
-  if (breakpoint.matches) {
-    dgAccordionMobileMenu.classList.add("guide-mobile-menu--hide");
-    dgGuideMenuBar.classList.remove("dg-guide__menu-bar--hide");
-  } else {
-    dgAccordionMobileMenu.classList.remove("guide-mobile-menu--hide");
-    dgGuideMenuBar.classList.add("dg-guide__menu-bar--hide");
+  if (!dgGuideMenuBar) return;
+  const dgAccordionMobileMenu = document.querySelector(".guide-mobile-menu");
+  const dgAccordionContent = dgAccordionMobileMenu.querySelector(
+    ".usa-accordion__content"
+  );
+  const desktopBreakpoint = window.matchMedia("(min-width: 480px)");
+
+  /**
+   *  Reset menu based on screen size
+   *
+   *  @param {MediaQueryList} breakpoint
+   */
+  function handleResize(breakpoint) {
+    if (breakpoint.matches) {
+      dgAccordionMobileMenu.classList.add("guide-mobile-menu--hide");
+      dgGuideMenuBar.classList.remove("dg-guide__menu-bar--hide");
+    } else {
+      dgAccordionMobileMenu.classList.remove("guide-mobile-menu--hide");
+      dgGuideMenuBar.classList.add("dg-guide__menu-bar--hide");
+    }
   }
-}
 
-/**
- * Update usa-accordion content to hidden on page load
- */
-document.addEventListener("DOMContentLoaded", () => {
-  dgAccordionContent.setAttribute("hidden", true);
-});
+  /**
+   * Update usa-accordion content to hidden on page load
+   */
+  document.addEventListener("DOMContentLoaded", () => {
+    dgAccordionContent.setAttribute("hidden", true);
+  });
 
-/**
- * Initialize guide functions for toggling guide menu
- */
-function initGuideMobileMenu() {
-  handleResize(desktopBreakpoint);
-  desktopBreakpoint.addEventListener("change", handleResize);
-}
+  /**
+   * Initialize guide functions for toggling guide menu
+   */
+  function initGuideMobileMenu() {
+    handleResize(desktopBreakpoint);
+    desktopBreakpoint.addEventListener("change", handleResize);
+  }
 
-initGuideMobileMenu();
+  initGuideMobileMenu();
+})();
