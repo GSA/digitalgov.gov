d68e67961b4f00b1e54c78ca4214eefbcd1eb5fc
lcummings3424 <liam.cummings@gsa.gov>
2024-07-31 14:20:56 -0500

    Inital usa-card refactoring

--- config/gulp/upload.js
+++ config/gulp/upload.js
@@ -47,7 +47,7 @@ const fileDir = "content/uploads/_working-files/to-process/";
 async function uploadImage(filePath, fileName) {
     try {
         const fileContent = await fs.promises.readFile(filePath);
-        let contentType = mimeTypes.lookup(filePath) || "application/octet-stream";
+        const contentType = mimeTypes.lookup(filePath) || "application/octet-stream";
         await client.send(new AWS.PutObjectCommand({
             Bucket: bucketName,
             Key: fileName,
@@ -126,10 +126,10 @@ async function cleanup() {
  * Initiates the upload process for both images and files
  */
 const upload = async () => {
-    let imageDirExists = fs.existsSync(imageDir);
-    let imageFiles = imageDirExists ? fs.readdirSync(imageDir) : [];
-    let fileDirExists = fs.existsSync(fileDir);
-    let fileFiles = fileDirExists ? fs.readdirSync(fileDir) : [];
+    const imageDirExists = fs.existsSync(imageDir);
+    const imageFiles = imageDirExists ? fs.readdirSync(imageDir) : [];
+    const fileDirExists = fs.existsSync(fileDir);
+    const fileFiles = fileDirExists ? fs.readdirSync(fileDir) : [];
     try {
         // Upload image files
         for (const file of imageFiles) {
